0094483caed47cbebb1b87511ef45e44e9cced97
Antonio Sánchez <sago.ant@gmail.com>
2023-08-25 22:02:02 +0200

    feat: query sample response

---
+++ nyt.js
@@ -0,0 +1,9 @@
+const API_KEY = ""
+
+const main = async () => {
+    const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${API_KEY}&published_date=2023-08-21`)
+    const body = await response.json()
+    console.log(JSON.stringify(body))
+} 
+
+main()
