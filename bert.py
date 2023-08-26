59d0de974d65963a6ba4d5ec263e0b9522fb68a4
Antonio Sánchez <sago.ant@gmail.com>
2023-08-26 16:55:41 +0200

    feat: performing semantic search

--- bert.py
+++ bert.py
@@ -1,10 +1,22 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 from json import load
 
-dbFile = open('./db.json')
-books = load(dbFile) 
-
 model = SentenceTransformer('all-MiniLM-L6-v2')
+dbFile = open('./db.json')
 
-embedding = model.encode(books['9781649374042']['description'])
-print(embedding)
+books = [*load(dbFile).values()][0:5]
+descriptions = [book['description'] for book in books]
+embeddings = model.encode(descriptions, convert_to_tensor=True)
+
+def convert_to_book(hit):
+    return {
+        'isbn': books[hit['corpus_id']]['primary_isbn13'],
+        'score': hit['score']
+    }
+def convert_hits(hits):
+    return [convert_to_book(hit) for hit in hits]
+
+hits = util.semantic_search(embeddings, embeddings, top_k=3)
+augmented = [convert_hits(hit) for hit in hits]
+
+print(augmented)
