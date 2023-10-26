80e495bf56e188c8365467040d3c2e7ba1c59e1b
Antonio Sánchez <sago.ant@gmail.com>
2023-10-26 15:43:58 +0000

    refactor: integrated server and ui tests

--- site/src/routes/[isbn]/page.test.ts
+++ site/src/routes/[isbn]/page.test.ts
@@ -1,22 +1,31 @@
 import '@testing-library/jest-dom/vitest'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import {render, screen} from '@testing-library/svelte'
+import books from '../../../../data/books.json'
+import relationships from '../../../../data/relationships.json'
+import { load } from './+page.server'
 import page from './+page.svelte'
 
-const ISBN = '666'
-const BOOK = { title: 'foo', author: 'bar', description: 'foobar', primary_isbn13: ISBN }
-const RELATIONSHIPS = ['1', '2']
+vi.mock('../../../../data/books.json', () => ({ default: { 666: { title: 'foo', primary_isbn13: 666 }}}))
+vi.mock('../../../../data/relationships.json', () => ({ default: { 666: [{ isbn: '1', score: '0.2' }, { isbn: '2', score: '0.3' }] } }))
 
 describe('render detail page', () => {
 	it('shows book details', async () => {
-		render(page, { data: { book: BOOK, recommended: RELATIONSHIPS } })
+		const ISBN = '666'
+		const BOOK = books[ISBN]
+		const RELATIONSHIPS = relationships[ISBN]
+
+		render(page, { data: await load({ params: { isbn: ISBN }} as any) })
 
 		const heading = screen.getByRole('heading')
 		const description = screen.getByRole('main')
 		const isbn = screen.getByRole('note')
+		const recommended = screen.getAllByRole('listitem')
 
 		expect(heading).toHaveTextContent(`${BOOK.title} by ${BOOK.author}`);
 		expect(description).toHaveTextContent(BOOK.description);
 		expect(isbn).toHaveTextContent(BOOK.primary_isbn13);
+		expect(recommended[0]).toHaveTextContent(RELATIONSHIPS[1].isbn);
+		expect(recommended[1]).toHaveTextContent(RELATIONSHIPS[0].isbn);
 	});
 });
