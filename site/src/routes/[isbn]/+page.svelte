7de93f93cfc3afa512793a222708bd5571d099ac
Antonio Sánchez <sago.ant@gmail.com>
2023-10-26 18:12:39 +0000

    feat: added buy links

--- site/src/routes/[isbn]/+page.svelte
+++ site/src/routes/[isbn]/+page.svelte
@@ -5,6 +5,7 @@
 <h1>{data.book.title} by {data.book.author}</h1>
 <p role='main'>{data.book.description}</p>
 <p role='note'>ISBN {data.book.primary_isbn13}</p>
+<a href={data.book.amazon_product_url}>Buy now!</a>
 
 <ul>
 	{#each data.recommended as isbn}
--- site/src/routes/[isbn]/page.test.ts
+++ site/src/routes/[isbn]/page.test.ts
@@ -6,7 +6,7 @@ import relationships from '../../../../data/relationships.json'
 import { load } from './+page.server'
 import page from './+page.svelte'
 
-vi.mock('../../../../data/books.json', () => ({ default: { 666: { title: 'foo', primary_isbn13: 666 }}}))
+vi.mock('../../../../data/books.json', () => ({ default: { 666: { title: 'foo', primary_isbn13: 666, amazon_product_url: 'https://www.example.com/dp/1649374046' }}}))
 vi.mock('../../../../data/relationships.json', () => ({ default: { 666: [{ isbn: '1', score: '0.2' }, { isbn: '2', score: '0.3' }] } }))
 
 describe('render detail page', () => {
@@ -21,10 +21,12 @@ describe('render detail page', () => {
 		const description = screen.getByRole('main')
 		const isbn = screen.getByRole('note')
 		const recommended = screen.getAllByRole('listitem')
+		const buyLink = screen.getByRole('link', { name: 'Buy now!' })
 
 		expect(heading).toHaveTextContent(`${BOOK.title} by ${BOOK.author}`);
 		expect(description).toHaveTextContent(BOOK.description);
 		expect(isbn).toHaveTextContent(BOOK.primary_isbn13);
+		expect(buyLink).toHaveAttribute('href', BOOK.amazon_product_url);
 		expect(recommended[0]).toHaveTextContent(RELATIONSHIPS[1].isbn);
 		expect(recommended[1]).toHaveTextContent(RELATIONSHIPS[0].isbn);
 	});
