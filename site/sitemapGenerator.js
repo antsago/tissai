428d57fc1fed199bc7eea4a15ffe28e6a1ad28fd
Antonio Sánchez <sago.ant@gmail.com>
2023-11-06 15:52:34 +0000

    feat: added sitemap

---
+++ site/sitemapGenerator.js
@@ -0,0 +1,21 @@
+import books from "../data/books.json" assert { type: "json" }
+
+const domain = "https://www.wibnix.com"
+
+console.log(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${domain}/</loc>
+    <lastmod>2023-11-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+${Object.keys(books).map(isbn =>
+`  <url>
+    <loc>${domain}/${isbn}</loc>
+    <lastmod>2023-11-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>`).join("\n")}
+</urlset>
+`)
