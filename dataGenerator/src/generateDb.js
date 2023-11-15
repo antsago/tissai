b05504b6bc9566e77792f08f67e62c5d2b6bda42
Antonio Sánchez <sago.ant@gmail.com>
2023-11-15 15:04:58 +0000

    feat: first run with actual data

--- dataGenerator/src/generateDb.js
+++ dataGenerator/src/generateDb.js
@@ -1,14 +1,11 @@
-require("dotenv").config()
-const parseResponse = require("./parseResponse")
+const getUrls = require("./getUrls")
 
-const API_KEY = process.env.API_KEY
+// const DOMAINS = ['www.decathlon.es', "es.shein.com"]
+const DOMAINS = ['es.shein.com']
 
 const main = async () => {
-  const response = await fetch(
-    `https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${API_KEY}&published_date=2023-08-21`,
-  )
-  const body = await response.json()
-  console.log(JSON.stringify(parseResponse(body)))
+  const urls = await Promise.all(DOMAINS.map(domain => getUrls(domain)))
+  console.log(JSON.stringify(urls))
 }
 
 main()
