49878eefb2771767e3d1e583742acc42f03e74d2
Antonio Sánchez <sago.ant@gmail.com>
2023-11-15 15:50:11 +0000

    refactor: extracted Robots and Sitexml

--- dataGenerator/src/Parser.js
+++ dataGenerator/src/Parser.js
@@ -1,35 +1,39 @@
 const robotsParser = require('robots-parser')
 const { parseStringPromise: xmlParser } = require('xml2js')
 
+const Robots = function (url, text, productToken) {
+  const robots = robotsParser(url, text)
+  
+  return {
+    get sitemaps() {
+      return robots.getSitemaps()
+    },
+    isAllowed: (url) => robots.isAllowed(url, productToken)
+  }
+}
+
+const Sitexml = async function (text) {
+  const parsedXml = await xmlParser(text, { strict: true })
+  
+  return {
+    /* For siteindexes */
+    get isSiteindex () {
+      return !!parsedXml.sitemapindex
+    },
+    get sitemaps () {
+      return parsedXml.sitemapindex?.sitemap?.map(sitemap => sitemap?.loc?.[0])
+    },
+    /* For sitemaps */
+    get urls () {
+      return parsedXml.urlset?.url?.map(url => url?.loc?.[0])
+    }
+  }
+}
+
 const Parser = function (productToken) {
   return {
-    robots: (url, text) => {
-      const robots = robotsParser(url, text)
-
-      return {
-        get sitemaps() {
-          return robots.getSitemaps()
-        },
-        isAllowed: (url) => robots.isAllowed(url, productToken)
-      }
-    },
-    sitexml: async (text) => {
-      const parsedXml = await xmlParser(text, { strict: true })
-
-      return {
-        /* For siteindexes */
-        get isSiteindex () {
-          return !!parsedXml.sitemapindex
-        },
-        get sitemaps () {
-          return parsedXml.sitemapindex?.sitemap?.map(sitemap => sitemap?.loc?.[0])
-        },
-        /* For sitemaps */
-        get urls () {
-          return parsedXml.urlset?.url?.map(url => url?.loc?.[0])
-        }
-      }
-    }
+    robots: (url, text) => Robots(url, text, productToken),
+    sitexml: async (text) => await Sitexml(text)
   }
 }
 
