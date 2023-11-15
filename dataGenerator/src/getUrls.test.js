613cf3c9c5cc528a790bd5bffcd92c9d0d56e8ef
Antonio Sánchez <sago.ant@gmail.com>
2023-11-15 12:18:36 +0000

    feat: ensure crawler respects product token

--- dataGenerator/src/getUrls.test.js
+++ dataGenerator/src/getUrls.test.js
@@ -32,7 +32,7 @@ describe("getUrls", () => {
   it("filters disallowed urls", async () => {
     const url1 = `https://${DOMAIN}/url1`
     const url2 = `https://${DOMAIN}/url2/`
-    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nUser-Agent: *\nDisallow: /url2`
+    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nUser-Agent: Wibnix\nDisallow: /url2`
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${url1}</loc></url><url><loc>${url2}</loc></url></urlset>`
     response.mockResolvedValueOnce(robots).mockResolvedValueOnce(sitemap)
 
@@ -44,7 +44,7 @@ describe("getUrls", () => {
   it("supports siteindexes", async () => {
     const url1 = `https://${DOMAIN}/url1`
     const url2 = `https://${DOMAIN}/url2/`
-    const robots = `Sitemap: https://${DOMAIN}/siteindex.xml\nUser-Agent: *\nDisallow:`
+    const robots = `Sitemap: https://${DOMAIN}/siteindex.xml\nUser-Agent: Wibnix\nDisallow:`
     const siteindex = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <sitemap><loc>https://${DOMAIN}/sitemap1.xml</loc></sitemap>
       <sitemap><loc>https://${DOMAIN}/sitemap2.xml</loc></sitemap>
@@ -61,7 +61,7 @@ describe("getUrls", () => {
   it("ignores disallowed sitemaps", async () => {
     const url1 = `https://${DOMAIN}/url1`
     const url2 = `https://${DOMAIN}/url2/`
-    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nSitemap: https://${DOMAIN}/sitemap2.xml\nUser-Agent: *\nDisallow:/sitemap2`
+    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nSitemap: https://${DOMAIN}/sitemap2.xml\nUser-Agent: Wibnix\nDisallow:/sitemap2`
     const sitemap1 = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${url1}</loc></url></urlset>`
     const sitemap2 = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${url2}</loc></url></urlset>`
     response.mockResolvedValueOnce(robots).mockResolvedValueOnce(sitemap1).mockResolvedValueOnce(sitemap2)
@@ -74,7 +74,7 @@ describe("getUrls", () => {
   it("ignores disallowed siteindex", async () => {
     const url1 = `https://${DOMAIN}/url1`
     const url2 = `https://${DOMAIN}/url2/`
-    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nSitemap: https://${DOMAIN}/siteindex.xml\nUser-Agent: *\nDisallow:/siteindex`
+    const robots = `Sitemap: https://${DOMAIN}/sitemap.xml\nSitemap: https://${DOMAIN}/siteindex.xml\nUser-Agent: Wibnix\nDisallow:/siteindex`
     const siteindex = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <sitemap><loc>https://${DOMAIN}/sitemap2.xml</loc></sitemap>
     </sitemapindex>`
