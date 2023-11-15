35fac538901cac573ecbf23e3f4a06b3e4b042e5
Antonio Sánchez <sago.ant@gmail.com>
2023-11-15 15:40:05 +0000

    refactor: extacted parser

--- dataGenerator/src/getUrls.js
+++ dataGenerator/src/getUrls.js
@@ -1,22 +1,57 @@
 const robotsParser = require('robots-parser')
-const { parseStringPromise: parseXml } = require('xml2js')
+const { Parser: XmlParser } = require('xml2js')
 
 const AGENT_TOKEN = 'Wibnix/1.0'
 const headers = { UserAgent: AGENT_TOKEN }
+const xmlParser = new XmlParser()
+
+const Parser = function (productToken) {
+  return {
+    robots: (url, text) => {
+      const robots = robotsParser(url, text)
+
+      return {
+        get sitemaps() {
+          return robots.getSitemaps()
+        },
+        isAllowed: (url) => robots.isAllowed(url, productToken)
+      }
+    },
+    sitexml: async (text) => {
+      const parsedXml = await xmlParser.parseStringPromise(text)
+
+      return {
+        /* For siteindexes */
+        get isSiteindex () {
+          return !!parsedXml.sitemapindex
+        },
+        get sitemaps () {
+          return parsedXml.sitemapindex?.sitemap?.map(sitemap => sitemap?.loc?.[0])
+        },
+        /* For sitemaps */
+        get urls () {
+          return parsedXml.urlset?.url?.map(url => url?.loc?.[0])
+        }
+      }
+    }
+  }
+}
+
+const parse = new Parser(AGENT_TOKEN)
 
 const robotsTxt = async (domain) => {
   const robotsUrl = `https://${domain}/robots.txt`
   const response = await fetch(robotsUrl, { headers })
   const robotsText = await response.text()
 
-  return robotsParser(robotsUrl, robotsText)
+  return parse.robots(robotsUrl, robotsText)
 }
 
 const sitemap = async (sitemapUrl) => {
   try {
     const response = await fetch(sitemapUrl, { headers })
     const text = await response.text()
-    return parseXml(text)  
+    return parse.sitexml(text)
   } catch {
     return {}
   }
@@ -26,11 +61,11 @@ const getUrls = async (domain) => {
   try {
     const robots = await robotsTxt(domain)
 
-    const sitexmls = await Promise.all(robots.getSitemaps().filter(url => robots.isAllowed(url, AGENT_TOKEN)).map(sitemap))
+    const sitexmls = await Promise.all(robots.sitemaps.filter(url => robots.isAllowed(url)).map(sitemap))
 
-    const sitemaps = await Promise.all(sitexmls.map(xml => !xml.sitemapindex ? xml : xml.sitemapindex.sitemap.map(site => sitemap(site.loc[0]))).flat())
+    const sitemaps = await Promise.all(sitexmls.map(xml => !xml.isSiteindex ? xml : xml.sitemaps.map(url => sitemap(url))).flat())
 
-    return sitemaps.map(site => site?.urlset?.url.map(url => url?.loc?.[0])).flat().filter(url => url && robots.isAllowed(url, AGENT_TOKEN))
+    return sitemaps.map(site => site.urls).flat().filter(url => url && robots.isAllowed(url))
   } catch {
     return []
   }
