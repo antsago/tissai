2acc77e4b4dc8cb60faa48d4aa52f20b40e5f6a6
Antonio Sánchez <sago.ant@gmail.com>
2023-11-20 21:30:03 +0000

    feat: pass domain through cli

--- dataGenerator/src/generateDb.js
+++ dataGenerator/src/generateDb.js
@@ -1,13 +1,11 @@
 const Crawler = require("./Crawler")
 
-// const DOMAIN = "es.shein.com"
-const DOMAIN = "www.decathlon.es"
 const PRODUCT_TOKEN = "Wibnix/0.1"
 
-const main = async () => {
-  for await (const url of Crawler(DOMAIN, PRODUCT_TOKEN).getAllowedUrls()) {
+const main = async (domain) => {
+  for await (const url of Crawler(domain, PRODUCT_TOKEN).getAllowedUrls()) {
     console.log(url)
   }
 }
 
-main()
+main(process.argv[2])
