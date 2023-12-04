7e7b13ef4d71dc9f5a6ddd2be77d9124646189f2
Antonio Sánchez <sago.ant@gmail.com>
2023-12-04 15:13:50 +0000

    refactor: improved readability

--- dataGenerator/src/Indexer.js
+++ dataGenerator/src/Indexer.js
@@ -5,21 +5,21 @@ const Indexer = function () {
     return !!pageContent.jsonLD.product
   }
 
-  const createProduct = function (pageContent) {
-    const ldProduct = pageContent.jsonLD.product
+  const createProduct = function ({ jsonLD, openGraph, headings }) {
+    const ldProduct = jsonLD.product
 
     return {
       id: randomUUID(),
       name: ldProduct.name,
       description:
-        ldProduct.description ??
-        pageContent.openGraph.description ??
-        pageContent.headings.description,
+        ldProduct.description ?? openGraph.description ?? headings.description,
       image: ldProduct.image,
       brand: ldProduct.brand?.name,
-      sellers: [{
-        productUrl: pageContent.headings.canonical,
-      }]
+      sellers: [
+        {
+          productUrl: headings.canonical,
+        },
+      ],
     }
   }
 
