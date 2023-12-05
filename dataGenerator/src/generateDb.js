b7fa9f615e9aac89ed4cf198ad5c479f76dc73c5
Antonio Sánchez <sago.ant@gmail.com>
2023-12-05 16:57:29 +0000

    feat: more html content exploration

--- dataGenerator/src/generateDb.js
+++ dataGenerator/src/generateDb.js
@@ -12,6 +12,26 @@ const SHOPS = {
     icon: "https://sheinsz.ltwebstatic.com/she_dist/images/touch-icon-ipad-144-47ceee2d97.png",
     domain: "es.shein.com",
   },
+  ZALANDO: {
+    name: "Zalando",
+    icon: "https://mosaic02.ztat.net/rnd/ng/zalando-app-touch-icon-180-v1.png",
+    domain: "www.zalando.es",
+  },
+  ZARA: {
+    name: "Zara",
+    icon: "https://static.zara.net/stdstatic/5.20.0//images/favicon-32x32.png",
+    domain: "www.zara.com",
+  },
+  IN_GRAVITY: {
+    name: "Zara",
+    icon: "https://static.zara.net/stdstatic/5.20.0//images/favicon-32x32.png",
+    domain: "www.zara.com",
+  },
+  MIRAVIA: {
+    name: "Miravia",
+    icon: "https://img.mrvcdn.com/us/media/7bada92e11dd275f27772c5ee02194ae.png",
+    domain: "www.miravia.es",
+  }
 }
 
 const PRODUCT_TOKEN = "Wibnix/0.1"
@@ -49,6 +69,17 @@ const main = async (domain, dataFolder, limit) => {
 }
 
 // main(process.argv[2], process.argv[3], process.argv[4])
+const findHeaders = (node, path = []) => {
+  if (!Array.isArray(node)) {
+    if (node.type === "text" && node.headerLevel > 0) {
+      return [{ ...node, path }]
+    }
+  
+    return []
+  }
+
+  return node.flatMap((child, cIndex) => findHeaders(child, [...path, cIndex]))
+}
 
 const test = async function (dataFolder) {
   const domain = SHOPS.DECATHLON.domain
@@ -60,11 +91,14 @@ const test = async function (dataFolder) {
   })
 
   const content = await crawler.getContent(
-    "https://www.decathlon.es/es/p/mp/regatta/pantalones-repelentes-al-agua-new-action-para-mujer-negro/_/R-p-28a99149-5812-44f5-a6b9-6d36aec296aa",
+    // "https://www.decathlon.es/es/p/mp/regatta/pantalones-repelentes-al-agua-new-action-para-mujer-negro/_/R-p-28a99149-5812-44f5-a6b9-6d36aec296aa",
+    // "https://www.decathlon.es/es/p/camiseta-termica-interior-de-esqui-y-nieve-ninos-5-14-anos-wedze-ski-100/_/R-p-121230",
+    // "https://es.shein.com/Young-Girl-Bow-Front-Overlap-Front-Sweatshirt-&-Sweatpants-p-24511225-cat-2115.html",
+    "https://www.zalando.es/nike-performance-academy-23-track-suit-branded-chandal-blackwhite-n1242k083-q11.html",
   )
 
-  console.log(`${JSON.stringify(content.html)}`)
-  return content
+  const headers = findHeaders(content.html).sort((a, b) => a.headerLevel - b.headerLevel)
+  return [headers, content]
 }
 
 test(process.argv[2])
