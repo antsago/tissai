bc041842f0a4508b241e5521e7d0a24b91fbe0fc
Antonio Sánchez <sago.ant@gmail.com>
2024-01-02 12:09:40 +0000

    feat: defined env with shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    vim
+    nodejs_20
+    yarn
+    python311Packages.python
+    python311Packages.pip
+    python311Packages.transformers
+    python311Packages.torch
+  ];
+}
