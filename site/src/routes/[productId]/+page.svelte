6fd37c8a16b3493153a2dd9c176b46623ac91f5a
Antonio Sánchez <sago.ant@gmail.com>
2024-01-31 14:25:49 +0000

    fix: preserve img aspect ratio

--- site/src/routes/[productId]/+page.svelte
+++ site/src/routes/[productId]/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col grow px-6 py-6">
 	<img
-		class="h-80 object-cover rounded-lg ring-2 ring-stone-200/50"
+		class="aspect-square object-cover rounded-lg ring-2 ring-stone-200/50"
 		alt={data.name}
 		src={data.image}
 	/>
@@ -12,7 +12,6 @@
 		<h1 class="text-stone-800 uppercase text-lg">{data.name}</h1>
 		<p class="mt-4 text-stone-700 text-base" role="main">{data.description}</p>
 	</div>
-	<!-- sticky -->
 	<a
 		class="sticky bottom-6 mt-auto py-6 px-10 bg-amber-600 rounded-2xl border border-amber-700 text-amber-200 text-center"
 		href={data.sellers?.[0].productUrl}
