b114a3c033a194bbd2b8139389ccb0cc5008a3d1
Antonio Sánchez <sago.ant@gmail.com>
2024-02-01 16:39:18 +0000

    refactor: Logo as icon

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import "../app.css"
+	import Logo from "$lib/Logo.svelte"
 </script>
 
 <div class="min-h-screen flex flex-col w-full">
 	<nav
 		class="mb-6 flex items-center border-b border-orange-600/50 px-6 py-2 bg-orange-400"
 	>
-		<img class="w-10" alt="Tissai logo" src="/favicon.svg" />
+		<Logo class="w-10 text-orange-200" />
 	</nav>
 	<slot />
 </div>
