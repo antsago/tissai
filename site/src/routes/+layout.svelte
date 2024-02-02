3b91f3a384e2cb69272b7a349ffdeb05d8be5f20
Antonio Sánchez <sago.ant@gmail.com>
2024-02-02 12:32:26 +0000

    feat: added name to navbar

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -5,9 +5,10 @@
 
 <div class="min-h-screen flex flex-col w-full">
 	<nav
-		class="mb-6 flex items-center border-b border-orange-600/50 px-6 py-2 bg-orange-400"
+		class="mb-6 flex justify-center items-end pr-6 px-4 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-700"
 	>
-		<Logo class="w-10 text-orange-200" />
+		<Logo class="w-10" />
+		<span class="font-['Kalam'] text-2xl">Tissai</span>
 	</nav>
 	<slot />
 </div>
