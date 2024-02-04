fc6725e91bb6317263195a35976666a2c1605e2f
Antonio Sánchez <sago.ant@gmail.com>
2024-02-04 11:43:20 +0000

    feat: bigger nav in desktop

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 
 <div class="flex flex-col min-h-screen w-full bg-stone-50">
 	<nav
-		class="flex justify-center items-end pr-6 px-4 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
+		class="flex justify-center items-end md:justify-start px-6 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
 	>
-		<Logo class="w-10" />
-		<span class="font-['Kalam'] text-2xl">Tissai</span>
+		<Logo class="w-10 md:w-12" />
+		<span class="font-['Kalam'] text-2xl md:text-3xl">Tissai</span>
 	</nav>
 	<div class="min-h-6 grow"/>
 	<slot />
