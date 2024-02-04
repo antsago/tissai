ffeaf46ecba50f90e96cb95d77a83303495ad1f7
Antonio Sánchez <sago.ant@gmail.com>
2024-02-04 08:02:52 +0000

    feat: added footer

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -3,12 +3,17 @@
 	import Logo from "$lib/Logo.svelte"
 </script>
 
-<div class="min-h-screen flex flex-col w-full">
+<div class="flex flex-col min-h-screen w-full bg-stone-50">
 	<nav
-		class="mb-6 flex justify-center items-end pr-6 px-4 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
+		class="flex justify-center items-end pr-6 px-4 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
 	>
 		<Logo class="w-10" />
 		<span class="font-['Kalam'] text-2xl">Tissai</span>
 	</nav>
-	<slot />
+	<div class="my-6 flex flex-col">
+		<slot />
+	</div>
+	<div class="mt-auto flex justify-center bg-stone-200 px-4 py-4 text-sm text-stone-500">
+		<small class="">2024 @ Tissai</small>
+	</div>
 </div>
--- site/src/routes/[productId]/+page.svelte
+++ site/src/routes/[productId]/+page.svelte
@@ -4,7 +4,7 @@
 	export let data
 </script>
 
-<div class="flex flex-col grow space-y-6 md:flex-row md:grow-0 md:space-y-0">
+<div class="flex flex-col space-y-6 md:flex-row md:space-y-0">
 	<div class="bg-stone-100 md:grow md:max-w-md">
 		<img
 			class="w-full max-w-sm mx-auto border border-stone-200/50 aspect-square object-cover"
@@ -12,8 +12,8 @@
 			src={data.image}
 		/>
 	</div>
-	<div class="grow flex flex-col md:max-w-sm">
-		<div class="mb-6 bg-stone-200">
+	<div class="flex flex-col md:max-w-sm">
+		<div class="bg-stone-200">
 			<div class="max-w-prose mx-auto p-6">
 				<h1 class="text-stone-900 uppercase text-lg">{data.name}</h1>
 				<p class="mt-4 text-stone-700 text-base" role="main">{data.description}</p>
