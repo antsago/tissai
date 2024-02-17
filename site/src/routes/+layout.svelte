1d873b4320f01740f55dd7de7f60568ee0a72fec
Antonio Sánchez <sago.ant@gmail.com>
2024-02-17 07:54:50 +0000

    feat: basic tests for layout

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -4,18 +4,20 @@
 </script>
 
 <div class="flex flex-col min-h-screen w-full bg-stone-50">
-	<nav
+	<header
 		class="flex justify-center items-end md:justify-start px-6 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
 	>
 		<Logo class="w-10 md:w-12" />
 		<span class="font-['Kalam'] text-2xl md:text-3xl">Tissai</span>
-	</nav>
+	</header>
 	<div class="min-h-6 grow" />
-	<slot />
+	<main>
+		<slot />
+	</main>
 	<div class="min-h-6 grow-[2]" />
-	<div
+	<footer
 		class="mt-auto flex justify-center bg-stone-200 px-4 py-4 text-sm text-stone-500"
 	>
 		<small class="">2024 @ Tissai</small>
-	</div>
+	</footer>
 </div>
---
+++ site/src/routes/layout.test.ts
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom/vitest"
+import { describe, it, expect } from "vitest"
+import { render, screen } from "@testing-library/svelte"
+import page from "./+layout.svelte"
+
+describe("Layout", () => {
+	it("shows header, main, and footer", async () => {
+		render(page)
+		
+		const header = screen.getByRole('banner')
+		const main = screen.getByRole('main')
+		const footer = screen.getByRole('contentinfo')
+
+		expect(header).toBeInTheDocument()
+		expect(main).toBeInTheDocument()
+		expect(footer).toBeInTheDocument()
+	})
+})
