a2688c602b4d49d080cf560ad7f288ef70888c2a
Antonio Sánchez <sago.ant@gmail.com>
2024-02-18 10:49:44 +0000

    feat: improved spacing and sizing

--- site/src/routes/[productId]/+page.svelte
+++ site/src/routes/[productId]/+page.svelte
@@ -32,17 +32,17 @@
 </Section>
 
 <Section labelledBy="similar-products" class="mt-12 py-8 space-y-5">
-	<h2 id="similar-products" class="mx-8 text-stone-700 text-lg font-medium">Similares</h2>
-	<ul class="flex flex-row overflow-x-scroll space-x-12 px-8">
+	<h2 id="similar-products" class="mx-8 text-stone-700 text-xl font-medium">Similares</h2>
+	<ul class="flex flex-row overflow-x-scroll space-x-8 px-8">
 		{#each data.similar as recommendation}
-			<li class="min-w-64 max-w-64">
-				<a href="../{recommendation.id}" class="w-full block bg-stone-50 border-stone-300 border p-6 space-y-4 rounded">
+			<li class="min-w-56 max-w-56">
+				<a href="../{recommendation.id}" class="w-full block bg-stone-50 border-stone-300 border py-5 px-4 space-y-4 rounded">
 					<img
 						class="w-full aspect-square object-cover mx-auto rounded border-stone-200/50 border"
 						src={recommendation.image}
 						alt={recommendation.name}
 					/>
-					<h3 class="block w-full text-base text-center uppercase text-stone-700">
+					<h3 class="block w-full h-10 text-sm text-center uppercase text-stone-700 line-clamp-2">
 						{recommendation.name}
 					</h3>
 				</a>
