5ca3ce172e5ffb4910e87115ba393e2ebf07e617
Antonio Sánchez <sago.ant@gmail.com>
2024-02-18 08:31:48 +0000

    fix: handle larger images

--- site/src/routes/[productId]/+page.svelte
+++ site/src/routes/[productId]/+page.svelte
@@ -8,7 +8,7 @@
 <Section labelledBy="product-details" class="md:flex-row md:max-w-screen-md">
 	<div class="bg-stone-100 md:grow">
 		<img
-			class="w-full max-w-sm md:max-w-full mx-auto md:rounded-l border-y border-stone-200/50 aspect-square object-cover"
+			class="w-full max-w-sm md:max-w-4xl mx-auto md:rounded-l border-y border-stone-200/50 aspect-square object-cover"
 			alt={data.name}
 			src={data.images[0]}
 		/>
