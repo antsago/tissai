eedb6b878b0f30c36db23fee67ea33569368ef18
Antonio Sánchez <sago.ant@gmail.com>
2024-02-19 10:36:22 +0000

    feat: get results in real time

--- site/src/routes/search/+page.server.ts
+++ site/src/routes/search/+page.server.ts
@@ -1,24 +1,28 @@
 import type { PageServerLoad } from "./$types"
 import { PythonShell } from "python-shell"
 
-let concated = ""
 const echo = new PythonShell("./src/routes/search/echo.py", {
 	mode: "text",
 	pythonOptions: ["-u"], // get print results in real-time
 })
+let resolve: (message: string) => void
+let reject
 echo.on("message", (message) => {
-	concated = message
+	resolve(message)
 })
 echo.on("close", () => {
 	console.log("finished")
 })
-process.stdin.on("data", (data) => {
-	echo.send(data.toString())
-})
 
 export const load: PageServerLoad = async ({ url }) => {
 	const query = url.searchParams.get("q") || ""
+	const p = new Promise((res, rej) => {
+		resolve = res
+		reject = rej
+	})
+	
 	echo.send(query)
+	const concated = await p
 
 	return {
 		concated,
