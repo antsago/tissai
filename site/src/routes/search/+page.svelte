3a4678dbf3fc34994c968ef0a3d75aba3df0fd5b
Antonio Sánchez <sago.ant@gmail.com>
2024-02-26 09:44:44 +0000

    feat: reactive nº columns

--- site/src/routes/search/+page.svelte
+++ site/src/routes/search/+page.svelte
@@ -9,27 +9,39 @@
 		align-content: space-around;
 		/* Your container needs a fixed height, and it 
 		* needs to be taller than your tallest column. */
-		height: 400vh; 
+		height: 500vh; 
 	}
-
-	/* Re-order items into 4 rows */
-	/* .item:nth-of-type(4n+1) { order: 1; }
-	.item:nth-of-type(4n+2) { order: 2; }
-	.item:nth-of-type(4n+3) { order: 3; }
-	.item:nth-of-type(4n)   { order: 4; } */
-
-	/* Re-order items into 5 rows */
-	.item:nth-of-type(5n+1) { order: 1; }
-	.item:nth-of-type(5n+2) { order: 2; }
-	.item:nth-of-type(5n+3) { order: 3; }
-	.item:nth-of-type(5n+4) { order: 4; }
-	.item:nth-of-type(5n)   { order: 5; }
 
 	/* Force new columns */
 	.break {
 		flex-basis: 100%;
 		width: 0;
 		margin: 0;
+	}
+
+	/* Re-order items into 2 rows */
+	.item:nth-of-type(2n+1) { order: 1; }
+	.item:nth-of-type(2n) { order: 2; }
+	/* Re-order items into 3 rows */
+	@media (min-width: 640px) {
+		.item:nth-of-type(3n+1) { order: 1; }
+		.item:nth-of-type(3n+2) { order: 2; }
+		.item:nth-of-type(3n) { order: 3; }
+	}
+	/* Re-order items into 4 rows */
+	@media (min-width: 768px) {
+		.item:nth-of-type(4n+1) { order: 1; }
+		.item:nth-of-type(4n+2) { order: 2; }
+		.item:nth-of-type(4n+3) { order: 3; }
+		.item:nth-of-type(4n)   { order: 4; }
+	}
+	/* Re-order items into 5 rows */
+	@media (min-width: 1024px) {
+		.item:nth-of-type(5n+1) { order: 1; }
+		.item:nth-of-type(5n+2) { order: 2; }
+		.item:nth-of-type(5n+3) { order: 3; }
+		.item:nth-of-type(5n+4) { order: 4; }
+		.item:nth-of-type(5n)   { order: 5; }
 	}
 </style>
 
@@ -49,8 +61,8 @@
 			</div>
 		{/each}
 		<span class="item break"></span>
-		<span class="item break"></span>
-		<span class="item break"></span>
-		<span class="item break"></span>
+		<span class="item break hidden sm:block"></span>
+		<span class="item break hidden md:block"></span>
+		<span class="item break hidden lg:block"></span>
 	</div>
 </section>
