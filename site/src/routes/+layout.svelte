11b2e902204e3ca0a992d36d8498a2f4bb21031d
Antonio Sánchez <sago.ant@gmail.com>
2024-03-07 15:29:05 +0000

    feat: link to homepage

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -9,10 +9,10 @@
 		class="px-6 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
 	>
 		<div class="max-w-screen-lg mx-auto flex justify-between items-center">
-			<div class="mr-6 flex items-end">
+			<a href="/" class="mr-6 flex items-end">
 				<Logo class="w-11 md:w-12" />
 				<span class="hidden md:block font-['Kalam'] text-3xl md:text-3xl">Tissai</span>
-			</div>
+			</a>
 
 			<SearchForm class="flex-grow max-w-lg"/>
 		</div>
