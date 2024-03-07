68b327bd98893df718c222f83c437d2a48f63a74
Antonio Sánchez <sago.ant@gmail.com>
2024-03-07 11:25:12 +0000

    feat: styled tile title

--- site/src/routes/search/+page.svelte
+++ site/src/routes/search/+page.svelte
@@ -8,14 +8,14 @@
 <Section label="Resultados de la búsqueda">
 	<Masonry class="content-center" tiles={data.products} let:tile={product}>
 		<a class="w-full h-full flex flex-col rounded bg-stone-200" href="../product/{product.id}">
-			<div class="flex-grow max-h-96">
+			<div class="w-full flex-grow max-h-96">
 				<img
 					src={product.image}
 					alt={product.name}
 					class="rounded border border-stone-300/50 object-contain"
 				/>
 			</div>
-			<h3 class="text-center p-2">{product.name}</h3>
+			<h3 class="block min-h-[1.5rem] max-h-[4.5rem] mx-4 my-3 text-base text-center text-ellipsis overflow-hidden text-stone-700">{product.name}</h3>
 		</a>
 	</Masonry>
 </Section>
