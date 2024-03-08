f66f989c2282457f581674b7c3e1f4f2ff2c060a
Antonio Sánchez <sago.ant@gmail.com>
2024-03-08 14:18:24 +0000

    refactor: extracted header

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
 	import "../app.css"
 	import { page } from "$app/stores"
-	import { Logo } from "$lib/components"
-	import SearchForm from "./SearchForm.svelte"
+	import Header from "./Header.svelte"
 
 	const HOMEPAGE = "/"
 </script>
 
 <div class="flex flex-col min-h-screen w-full bg-stone-50">
 	{#if $page.url.pathname !== HOMEPAGE}
-		<header
-			class="px-6 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
-		>
-			<div class="max-w-screen-lg mx-auto flex justify-between items-center">
-				<a href="/" class="mr-6 flex items-end">
-					<Logo class="w-11 md:w-12" />
-					<span class="hidden md:block font-['Kalam'] text-3xl md:text-3xl"
-						>Tissai</span
-					>
-				</a>
-
-				<SearchForm class="flex-grow max-w-lg" initialValue={$page.url.searchParams.get("q")} />
-			</div>
-		</header>
+		<Header searchQuery={$page.url.searchParams.get("q")} />
 	{/if}
 
 	<div class="min-h-12 grow" />
---
+++ site/src/routes/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Logo } from "$lib/components"
+	import SearchForm from "./SearchForm.svelte"
+
+	export let searchQuery: string | null
+</script>
+
+<header
+	class="px-6 py-2 bg-orange-100 border-b-2 border-orange-700 text-orange-600"
+>
+	<div class="max-w-screen-lg mx-auto flex justify-between items-center">
+		<a href="/" class="mr-6 flex items-end">
+			<Logo class="w-11 md:w-12" />
+			<span class="hidden md:block font-['Kalam'] text-3xl md:text-3xl"
+				>Tissai</span
+			>
+		</a>
+
+		<SearchForm class="flex-grow max-w-lg" initialValue={searchQuery} />
+	</div>
+</header>
