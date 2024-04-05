4f7fa083c6daaa2fda42b548b3a5687d35d5bf01
Antonio Sánchez <sago.ant@gmail.com>
2024-04-05 12:50:03 +0000

    refactor: extracted isMeaningless fn

--- dataGenerator/src/Labeler/labeler.py
+++ dataGenerator/src/Labeler/labeler.py
@@ -2,9 +2,13 @@ import spacy
 
 nlp = spacy.load('es_core_news_sm', exclude=['morphologizer', 'parser', 'attribute_ruler', 'lemmatizer', 'ner'])
 
+isMeaningless = lambda token: not (
+  token.is_stop
+  or token.is_punct
+  or token.is_space or token.is_quote or token.is_bracket
+  or token.text == '|'
+)
+
 def getLabels(title):
   doc = nlp(title.lower())
-  return { token.text for token in doc if not (
-    token.is_stop or token.is_punct or token.is_space or token.is_quote or token.is_bracket
-    or token.text == '|'
-  )}
+  return { token.text for token in doc if isMeaningless(token) }
