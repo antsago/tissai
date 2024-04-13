e24ffb88d72ad41f234b9c581403bd8e5dd7beeb
Antonio Sánchez <sago.ant@gmail.com>
2024-04-13 06:30:37 +0000

    feat: creating categories

--- dataGenerator/src/generateLabels.py
+++ dataGenerator/src/generateLabels.py
@@ -1,12 +1,25 @@
 from Labeler import Labeler
-from products import products
 from collections import Counter
 from functools import reduce
 
 
-def countLabels(count, product):
+def addLabels(count, product):
     count.update(Labeler(product["name"]).getLabels())
     return count
 
+def countLabels(products):
+    count = reduce(addLabels, products, Counter())
+    return count
 
-count = reduce(countLabels, products, Counter())
+def addCategories(categories, product):
+    if product["category"] not in categories:
+        categories[product["category"]] = { "products": [], "labels": Counter() }
+    categories[product["category"]]["products"].append(product)
+    categories[product["category"]]["labels"].update(product["labels"])
+    return categories
+
+def countCategories(products):
+    labels = [Labeler(product["name"]) for product in products]
+    categorized = [{ **product, "category": label.getCategory(), "labels": label.getLabels()} for (product, label) in zip(products, labels)]
+    categories = reduce(addCategories, categorized, {})
+    return categories
