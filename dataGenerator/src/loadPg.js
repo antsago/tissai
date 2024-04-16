38faf3e241fba861079155cb9740f812c182d55c
Antonio Sánchez <sago.ant@gmail.com>
2024-04-16 12:36:52 +0000

    feat: load pages into uranus

--- dataGenerator/src/loadPg.js
+++ dataGenerator/src/loadPg.js
@@ -1,7 +1,9 @@
 const { randomUUID } = require("node:crypto")
+const { readdir, readFile } = require("fs/promises")
 const { Pool, escapeLiteral } = require("pg")
-const products = require("../../data/withEmbeddings.json")
-const shops = require("./shops")
+// const products = require("../../data/withEmbeddings.json")
+const { Content } = require("./Crawler/parsers")
+// const shops = require("./shops")
 
 function escape(literal) {
   if (literal === null || literal === undefined) {
@@ -60,6 +62,62 @@ async function loadProducts() {
   await pool.end()
 }
 
+async function loadPageFromSite(site, path, pool) {
+  const file = await readFile(`../data/${site.domain}/${path}`, {
+    encoding: "utf8",
+  })
+  const request = JSON.parse(file)
+  const content = Content(request.url, request.body)
+  
+  if (
+    !content?.jsonLD?.product &&
+    content?.openGraph?.type !== "product"
+  ) { return }
+  
+  const query = `
+    INSERT INTO pages (id, url, body, site)
+    VALUES (
+      ${escape(randomUUID())},
+      ${escape(request.url)},
+      ${escape(request.body)},
+      ${escape(site.id)}
+    );
+  `
+  await pool.query(query)
+}
+async function loadPagesFromSite(site, pool) {
+  const paths = await readdir(`../data/${site.domain}`)
+
+  await paths.reduce(async (previousPath, path) => {
+    await previousPath
+
+    await loadPageFromSite(site, path, pool)
+  }, Promise.resolve())
+}
+async function loadPages() {
+  const pool = new Pool({
+    connectionString: 'postgres://postgres:postgres@postgres:5432/uranus',
+  })
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS pages (
+      id                    uuid PRIMARY KEY,
+      url                   text UNIQUE NOT NULL,
+      body                  text NOT NULL,
+      site                  uuid REFERENCES sites
+    );`)
+  console.log("Table created")
+
+  const sites = (await pool.query(`SELECT id, domain FROM sites ORDER BY domain LIMIT 1 OFFSET 8;`)).rows
+
+  await sites.reduce(async (previousSite, site) => {
+    await previousSite
+    await loadPagesFromSite(site, pool)
+    console.log(`Inserted pages for ${site.domain}`)
+  }, Promise.resolve())
+
+  await pool.end()
+}
+
 async function loadSites() {
   const initialPool = new Pool({
     connectionString: process.env.PG_CONNECTION_STRING,
@@ -81,14 +139,7 @@ async function loadSites() {
       sitemapWhitelist      text[],
       urlKeywords           text[]
     );`)
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS pages (
-      id                    uuid PRIMARY KEY,
-      url                   text UNIQUE NOT NULL,
-      body                  text NOT NULL,
-      site                  uuid REFERENCES sites
-    );`)
-  console.log("Tables created")
+  console.log("Table created")
 
   const values = shops.map((shop) => {
     return `(
@@ -116,3 +167,4 @@ async function loadSites() {
 
 // loadProducts()
 // loadSites()
+loadPages()
