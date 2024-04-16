980d36a4c84ac02de738e65d7c43c26be48c3cc3
Antonio Sánchez <sago.ant@gmail.com>
2024-04-16 08:00:40 +0000

    feat: better index test

--- arachne/src/index.ts
+++ arachne/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hola")
+import robotsParser from "robots-parser"
+import { Db } from "./Db.js"
+import { Cache } from "./Cache.js"
+
+const CACHE_FOLDER = process.argv[2]
+const cache = Cache(CACHE_FOLDER)
+const db = Db()
+
+const site = (await db.query("SELECT * FROM sites"))[0]
+
+const robotsResponse = await cache.get(`https://${site.domain}/robots.txt`)
+const robots = robotsParser(robotsResponse.url, robotsResponse.body)
+const sitemaps = robots.getSitemaps()
+
+db.query(sitemaps[0])
