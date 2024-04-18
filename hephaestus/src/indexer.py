caba58f11ad124db369dde99c9ac83b8b17b00e6
Antonio Sánchez <sago.ant@gmail.com>
2024-04-18 07:59:54 +0000

    feat: first tests

--- hephaestus/src/indexer.py
+++ hephaestus/src/indexer.py
@@ -1,2 +1,10 @@
-def parse():
-  return True
+import json
+from bs4 import BeautifulSoup
+
+def parse(page):
+  soup = BeautifulSoup(page, 'lxml')
+
+  jsonString = soup.find_all(name="script", type="application/ld+json")[0].string
+  linkedData = json.loads(jsonString)
+
+  return linkedData
--- hephaestus/src/indexer_test.py
+++ hephaestus/src/indexer_test.py
@@ -1,6 +1,20 @@
+import json
 from indexer import parse
 
 
 def test_extracts_json_ld_products():
-    result = parse()
-    assert result == True
+    product = {
+      "@context": "https://schema.org/",
+      "@type": "Product",
+      "name": "The name of the product",
+    }
+    page = f"""
+        <html>
+          <head>
+            <script type=\"application/ld+json\">{json.dumps(product)}</script>
+            <script src=\"_ascript\"></script>
+          </head>
+        </html>
+    """
+    result = parse(page)
+    assert result == product
