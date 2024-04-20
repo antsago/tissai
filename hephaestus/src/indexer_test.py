0bed35e3a39f6c3a797d4c21a0bb9fd07a1519eb
Antonio Sánchez <sago.ant@gmail.com>
2024-04-20 07:00:42 +0000

    refactor: used mock rather than magicmock

--- hephaestus/src/indexer_test.py
+++ hephaestus/src/indexer_test.py
@@ -1,5 +1,5 @@
 from asymmetric_matchers import anything
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 from __tests__ import productSchema, orgSchema
 import indexer
 
@@ -11,35 +11,35 @@ expectedProduct = {
 }
 
 def test_converts_jsonld_to_product():
-    page = MagicMock(jsonLd = [productSchema])
+    page = Mock(jsonLd = [productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct]
 
 def test_ignores_non_product():
-    page = MagicMock(jsonLd = [orgSchema, productSchema])
+    page = Mock(jsonLd = [orgSchema, productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct]
 
 def test_handles_pages_with_multiple_products():
-    page = MagicMock(jsonLd = [productSchema, productSchema])
+    page = Mock(jsonLd = [productSchema, productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct, expectedProduct]
 
 def test_handles_pages_without_product():
-    page = MagicMock(jsonLd = [orgSchema])
+    page = Mock(jsonLd = [orgSchema])
 
     result = indexer.toProduct(page)
 
     assert result == []
 
 def test_handles_images_array():
-    page = MagicMock(jsonLd = [{
+    page = Mock(jsonLd = [{
         **productSchema,
         "image": [productSchema["image"]],
     }])
