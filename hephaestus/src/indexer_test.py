b1849283ecee2c3e07183fd0199dab6c7715c3a2
Antonio Sánchez <sago.ant@gmail.com>
2024-04-20 07:07:03 +0000

    Revert "refactor: used mock rather than magicmock"
    This reverts commit 0bed35e3a39f6c3a797d4c21a0bb9fd07a1519eb.

--- hephaestus/src/indexer_test.py
+++ hephaestus/src/indexer_test.py
@@ -1,5 +1,5 @@
 from asymmetric_matchers import anything
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 from __tests__ import productSchema, orgSchema
 import indexer
 
@@ -11,35 +11,35 @@ expectedProduct = {
 }
 
 def test_converts_jsonld_to_product():
-    page = Mock(jsonLd = [productSchema])
+    page = MagicMock(jsonLd = [productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct]
 
 def test_ignores_non_product():
-    page = Mock(jsonLd = [orgSchema, productSchema])
+    page = MagicMock(jsonLd = [orgSchema, productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct]
 
 def test_handles_pages_with_multiple_products():
-    page = Mock(jsonLd = [productSchema, productSchema])
+    page = MagicMock(jsonLd = [productSchema, productSchema])
 
     result = indexer.toProduct(page)
 
     assert result == [expectedProduct, expectedProduct]
 
 def test_handles_pages_without_product():
-    page = Mock(jsonLd = [orgSchema])
+    page = MagicMock(jsonLd = [orgSchema])
 
     result = indexer.toProduct(page)
 
     assert result == []
 
 def test_handles_images_array():
-    page = Mock(jsonLd = [{
+    page = MagicMock(jsonLd = [{
         **productSchema,
         "image": [productSchema["image"]],
     }])
