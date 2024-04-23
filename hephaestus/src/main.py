d10f142a1fd0736d6986070669203ba449d1616c
Antonio Sánchez <sago.ant@gmail.com>
2024-04-23 08:06:23 +0000

    feat: print pageid on error

--- hephaestus/src/main.py
+++ hephaestus/src/main.py
@@ -2,10 +2,14 @@ import db
 from page import Page
 import indexer
 
-db.createGaiaDb()
+try:
+  db.createGaiaDb()
 
-for rawPage in db.getPages():
-  page = Page(rawPage)
-  products = indexer.toProduct(page)
-  for product in products:
-    db.loadProduct(product)
+  for rawPage in db.getPages():
+    page = Page(rawPage)
+    products = indexer.toProduct(page)
+    for product in products:
+      db.loadProduct(product)
+except Exception as error:
+  print(page.id)
+  raise error
--- hephaestus/src/main_test.py
+++ hephaestus/src/main_test.py
@@ -1,3 +1,5 @@
+import pytest
+import importlib
 from unittest.mock import call
 from asymmetric_matchers import string_matching, list_containing, dict_containing
 from __tests__ import MockPg, productSchema, pageForTest
@@ -20,3 +22,22 @@ def test_turns_pages_into_products():
     call(string_matching('INSERT INTO products'), dict_containing({ "page": page1["id"], })),
     call(string_matching('INSERT INTO products'), dict_containing({ "page": page2["id"], }))
   ]) == mocked.cursor.execute.call_args_list
+
+def test_prints_current_page_on_error(capsys):
+  mocked = MockPg()
+  page1 = {
+    **pageForTest([productSchema]),
+    "id": "page-id",
+  }
+  page2 = {
+    **pageForTest([productSchema]),
+    "id": "page-id-2",
+  }
+  mocked.cursor.fetchone.side_effect = [(page1["id"], page1["body"]), (page2["id"], page2["body"]), None]
+  mocked.cursor.execute.side_effect = [None, None, None, Exception("Booh!")]
+
+  with pytest.raises(Exception):
+    import main
+    importlib.reload(main)
+
+  assert capsys.readouterr().out == string_matching(page2["id"])
