d32540caac56126b573bdc131aea9ffe69df9f5e
Antonio Sánchez <sago.ant@gmail.com>
2024-04-29 15:49:07 +0000

    refactor: extracted common predicate values

--- hephaestus/src/predicates_test.py
+++ hephaestus/src/predicates_test.py
@@ -2,22 +2,9 @@ import json
 from asymmetric_matchers import string_matching, list_containing
 from predicates import createPredicates, Predicate
 
-productSchema = {
-  "@context": { "@vocab": "http://schema.org/" },
-  "@type": "Product",
-}
-productPredicates = [
-    Predicate(string_matching(".*"), "IRI", 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://schema.org/Product', "IRI"),
-]
-orgSchema = {
-  "@context": { "@vocab": "http://schema.org/" },
-  '@type': 'Organization',
-}
-orgPredicates = [
-    Predicate(string_matching(".*"), "IRI", 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://schema.org/Organization', "IRI"),
-]
+PAGE_ID = "test-id"
 pageForTest = lambda schemas: {
-    "id": "test-id",
+    "id": PAGE_ID,
     "body": f"""
         <html>
           <head>
@@ -27,6 +14,27 @@ pageForTest = lambda schemas: {
         </html>
     """,
 }
+predForTest = lambda predicate, object, objectType = "IRI": Predicate(
+    subject=string_matching(".*"),
+    subjectType="IRI",
+    predicate=predicate,
+    object=object,
+    objectType=objectType,
+)
+productSchema = {
+  "@context": { "@vocab": "http://schema.org/" },
+  "@type": "Product",
+}
+orgSchema = {
+  "@context": { "@vocab": "http://schema.org/" },
+  '@type': 'Organization',
+}
+productPredicates = [
+    predForTest('http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://schema.org/Product'),
+]
+orgPredicates = [
+    predForTest('http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://schema.org/Organization'),
+]
 
 def test_json_ld():
     rawPage = pageForTest([productSchema])
@@ -49,10 +57,8 @@ def test_non_string_json_ld():
             "@vocab": "http://schema.org/",
             "validFrom": { "@type": "Date"},
         },
-        "@type":"Offer",
         "seller":{
             "@type":"Organization",
-            "name":"DECATHLON"
         },
         "isFamilyFriendly": False,
         "itemCondition":"https://schema.org/NewCondition",
@@ -63,14 +69,13 @@ def test_non_string_json_ld():
     predicates = createPredicates(rawPage)
 
     assert list_containing([
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/isFamilyFriendly", False, "http://www.w3.org/2001/XMLSchema#boolean"),
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/itemCondition", "https://schema.org/NewCondition", "Value"),
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/price", 24.99, "http://www.w3.org/2001/XMLSchema#double"),
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/seller", string_matching(".*"), "IRI"),
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/validFrom", "2011-04-01", "http://schema.org/Date"),
-        Predicate(string_matching(".*"), "IRI", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://schema.org/Offer", "IRI"),
-        Predicate(string_matching(".*"), "IRI", "http://schema.org/name", "DECATHLON", "Value"),
-        Predicate(string_matching(".*"), "IRI", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://schema.org/Organization", "IRI"),
+        predForTest('http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://schema.org/Organization'),
+        predForTest("http://schema.org/isFamilyFriendly", False, "http://www.w3.org/2001/XMLSchema#boolean"),
+        predForTest("http://schema.org/itemCondition", "https://schema.org/NewCondition", "Value"),
+        predForTest("http://schema.org/price", 24.99, "http://www.w3.org/2001/XMLSchema#double"),
+        predForTest("http://schema.org/seller", string_matching(".*"), "IRI"),
+        predForTest("http://schema.org/validFrom", "2011-04-01", "http://schema.org/Date"),
+        predForTest("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://schema.org/Organization"),
     ]) == list(predicates)
 
 # def test_stores_page_id():
