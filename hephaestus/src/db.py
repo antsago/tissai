27c7f0f1354088d273139905794e92da65fdb8e6
Antonio Sánchez <sago.ant@gmail.com>
2024-04-30 06:46:16 +0000

    feat: predicate insertion

--- hephaestus/src/db.py
+++ hephaestus/src/db.py
@@ -19,13 +19,53 @@ def createGaiaDb():
       curs.execute("""
         CREATE TABLE IF NOT EXISTS predicates (
           id                     uuid PRIMARY KEY,
-          page                   uuid,
-          predicate              text,
-          subject_string_value   text,
+          page                   uuid NOT NULL,
+          predicate              text NOT NULL,
+          subject_value          text,
           subject_rfd_type       text,
-          subject_json_type      text,
-          object_string_value    text,
+          subject_is_string      boolean NOT NULL,
+          object_value           text,
           object_rfd_type        text,
-          object_json_type       text
+          object_is_string       boolean NOT NULL
         );
       """)
+
+def addPredicate(predicate):
+  toStore = {
+    "id": predicate.id,
+    "page": predicate.page,
+    "predicate": predicate.predicate,
+    "subject_value": predicate.subject,
+    "subject_rfd_type": predicate.subjectType,
+    "subject_is_string": True,
+    "object_value": predicate.object,
+    "object_rfd_type": predicate.objectType,
+    "object_is_string": True,
+  }
+  with psycopg2.connect("postgres://postgres:postgres@postgres:5432/gaia") as conn:
+    with conn.cursor() as curs:
+      curs.execute(
+      """
+        INSERT INTO predicates (
+          id,
+          page,
+          predicate,
+          subject_value,
+          subject_rfd_type,
+          subject_is_string,
+          object_value,
+          object_is_string,
+          object_rfd_type
+        ) VALUES (
+          %(id)s,
+          %(page)s,
+          %(predicate)s,
+          %(subject_value)s,
+          %(subject_rfd_type)s,
+          %(subject_is_string)s,
+          %(object_value)s,
+          %(object_is_string)s,
+          %(object_rfd_type)s
+        );
+      """,
+      toStore)
--- hephaestus/src/db_test.py
+++ hephaestus/src/db_test.py
@@ -2,6 +2,7 @@ import importlib
 from unittest.mock import call
 from asymmetric_matchers import string_matching
 from __tests__ import MockPg
+from predicates import Predicate
 import db
 
 def test_configures_psycopg():
@@ -26,6 +27,32 @@ def test_getPages():
   result = list(db.getPages())
 
   assert result == [page1, page2]
+
+def test_insertPredicate():
+  mocked = MockPg()
+  predicate = Predicate(
+      id="id",
+      page="page-id",
+      subject="Subject",
+      predicate="Predicate",
+      object="Object",
+      subjectType="IRI",
+      objectType="IRI",
+  )
+
+  db.addPredicate(predicate)
+
+  assert [call(string_matching('INSERT INTO predicates'), {
+    "id": "id",
+    "page": "page-id",
+    "predicate": "Predicate",
+    "subject_value": "Subject",
+    "subject_rfd_type": "IRI",
+    "subject_is_string": True,
+    "object_value": "Object",
+    "object_rfd_type": "IRI",
+    "object_is_string": True,
+  })] == mocked.cursor.execute.call_args_list
 
 def test_dbCreation():
   mocked = MockPg()
