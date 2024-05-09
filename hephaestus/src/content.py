1d977c8502fb10b5702af65c460ae1b3f5c5cf0b
Antonio Sánchez <sago.ant@gmail.com>
2024-05-09 07:05:29 +0000

    feat: simple parsing of opengraph

--- hephaestus/src/content.py
+++ hephaestus/src/content.py
@@ -1,3 +1,4 @@
+import re
 import json
 from bs4 import BeautifulSoup
 
@@ -5,7 +6,12 @@ def extractJsonLd(soup):
   ldTags = soup.find_all(name="script", type="application/ld+json")
   return [json.loads(tag.string) for tag in ldTags]
 
+def extractOpengraph(soup):
+  ogTags = soup.find_all(name="meta", property=re.compile("^og:"))
+  return {tag['property']: tag['content'] for tag in ogTags}
+
 class Content:
   def __init__(self, page):
     soup = BeautifulSoup(page["body"], 'lxml')
     self.jsonLd = extractJsonLd(soup)
+    self.opengraph = extractOpengraph(soup)
--- hephaestus/src/content_test.py
+++ hephaestus/src/content_test.py
@@ -1,5 +1,5 @@
 from content import Content
-from __tests__ import pageForTest
+from __tests__ import PAGE_ID, pageForTest
 
 productSchema = {
   "@context": { "@vocab": "http://schema.org/" },
@@ -9,9 +9,23 @@ productSchema = {
 def test_parse_json_ld():
   page = pageForTest([productSchema])
   content = Content(page)
-  assert [
-    {
-      "@context": { "@vocab": "http://schema.org/" },
-      "@type": "Product",
-    }
-  ] == content.jsonLd
+  assert [productSchema] == content.jsonLd
+
+def test_parse_opengraph():
+  opengraph = {
+    "og:type": "product",
+    "og:title": "Friend Smash Coin",
+  }
+  page = {
+    "id": PAGE_ID,
+    "body": f"""
+        <html>
+          <head>
+            {"".join([f'<meta property="{key}" content="{value}" />' for key, value in opengraph.items()])}
+            <script src=\"_ascript\"></script>
+          </head>
+        </html>
+    """,
+  }
+  content = Content(page)
+  assert opengraph == content.opengraph
