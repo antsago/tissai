3dca9f595789f9a0c396d4f14c536088c9d48d8b
Antonio Sánchez <sago.ant@gmail.com>
2024-05-09 09:14:04 +0000

    feat: simple parsing of headings

--- hephaestus/src/content.py
+++ hephaestus/src/content.py
@@ -10,8 +10,27 @@ def extractOpengraph(soup):
   ogTags = soup.find_all(name="meta", property=re.compile("^og:"))
   return {tag['property']: tag['content'] for tag in ogTags}
 
+
+def extractHeadings(soup):
+  title = soup.head.title
+  description = soup.find("meta", {"name": "description"})
+  keywords = soup.find("meta", {"name": "keywords"})
+  author = soup.find("meta", {"name": "author"})
+  robots = soup.find("meta", {"name": "robots"})
+  canonical = soup.find(name="link", rel="canonical")
+
+  return {
+    "title": title.string if title != None else None,
+    "description": description["content"] if description != None else None,
+    "keywords": keywords["content"] if keywords != None else None,
+    "author": author["content"] if author != None else None,
+    "robots": robots["content"] if robots != None else None,
+    "canonical": canonical["href"] if canonical != None else None,
+  }
+
 class Content:
   def __init__(self, page):
     soup = BeautifulSoup(page["body"], 'lxml')
     self.jsonLd = extractJsonLd(soup)
     self.opengraph = extractOpengraph(soup)
+    self.headings = extractHeadings(soup)
--- hephaestus/src/content_test.py
+++ hephaestus/src/content_test.py
@@ -29,3 +29,32 @@ def test_parse_opengraph():
   }
   content = Content(page)
   assert opengraph == content.opengraph
+
+def test_parse_headings():
+  headings = {
+    "title": "The page title",
+    "description": "The description",
+    "keywords": "Some, keywords",
+    "author": "The author",
+    "robots": "index,follow",
+    "canonical": "https://example.com/url1",
+  }
+  page = {
+    "id": PAGE_ID,
+    "body": f"""
+        <html>
+          <head>
+            <title>{headings["title"]}</title>
+            <meta name="viewport" content="something else">
+            <meta name="description" content="{headings["description"]}">
+            <meta name='keywords' content='{headings["keywords"]}'>
+            <meta name='author' content='{headings["author"]}'>
+            <meta name="robots" content="{headings["robots"]}">
+            <link rel="canonical" href="{headings["canonical"]}" />
+            <script src=\"_ascript\"></script>
+          </head>
+        </html>
+    """,
+  }
+  content = Content(page)
+  assert headings == content.headings
