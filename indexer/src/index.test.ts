cf3dc601b3b8330fd9804e5e24e156d62c87c606
Antonio Sánchez <sago.ant@gmail.com>
2024-05-17 07:44:30 +0000

    feat: extract seller details

--- indexer/src/index.test.ts
+++ indexer/src/index.test.ts
@@ -1,37 +1,45 @@
 import { expect, describe, it, beforeEach, vi } from 'vitest'
-import { MockPg } from '#mocks'
+import type { MockPg } from '#mocks'
+
+const PRODUCT = {
+  title: "Test product",
+  description: "Product description",
+  image: "https://example.com/product-image.jpg"
+}
+const OFFER = {
+  seller: "Test Seller",
+  price: 70,
+  curency: "EUR",
+}
+const PAGE = {
+  id: "test_id",
+  body: `
+    <html>
+      <head>
+        <script type="application/ld+json">
+        ${JSON.stringify({
+          "@context": "https://schema.org/",
+          "@type": "Product",
+          "name": PRODUCT.title,
+          "description": PRODUCT.description,
+          "image": PRODUCT.image,
+        })}
+        </script>
+      </head>
+    </html>
+  `,
+  url: "https://example.com/product",
+  site: "site-id",
+}
 
 describe('indexer', () => {
   let pg: MockPg
-  beforeEach(() => {
+  beforeEach(async () => {
+    vi.resetModules()
+
+    const { MockPg } = await import('#mocks')
     pg = MockPg()
   })
-
-  const PRODUCT = {
-    title: "Test product",
-    description: "Product description",
-    image: "https://example.com/product-image.jpg"
-  }
-  const PAGE = {
-    id: "test_id",
-    body: `
-      <html>
-        <head>
-          <script type="application/ld+json">
-          ${JSON.stringify({
-            "@context": "https://schema.org/",
-            "@type": "Product",
-            "name": PRODUCT.title,
-            "description": PRODUCT.description,
-            "image": PRODUCT.image,
-          })}
-          </script>
-        </head>
-      </html>
-    `,
-    url: "https://example.com/product",
-    site: "site-id",
-  }
 
   it("extracts product and offer", async () => {
     pg.query.mockResolvedValue({ rows: [] }) 
@@ -41,5 +49,52 @@ describe('indexer', () => {
 
     expect(pg.query).toHaveBeenCalledWith(expect.stringContaining('INSERT INTO products'), expect.arrayContaining([PRODUCT.title, PRODUCT.description, PRODUCT.image]))
     expect(pg.query).toHaveBeenCalledWith(expect.stringContaining('INSERT INTO offers'), expect.arrayContaining([PAGE.site, PAGE.url]))
+  })
+
+  it("extracts offer details", async () => {
+    const PAGE2 = {
+      id: "test_id",
+      body: `
+        <html>
+          <head>
+            <script type="application/ld+json">
+            ${JSON.stringify({
+              "@context": "https://schema.org/",
+              "@type": "Product",
+              "name": PRODUCT.title,
+              "description": PRODUCT.description,
+              "image": PRODUCT.image,
+              "offers": {
+                "@type": "Offer",
+                "url": "https://example.com/offer",
+                "price": OFFER.price,
+                "priceCurrency": OFFER.curency,
+                "seller": {
+                  "@type": "Organization",
+                  "name": OFFER.seller,
+                }
+              }
+            })}
+            </script>
+          </head>
+        </html>
+      `,
+      url: "https://example.com/product",
+      site: "site-id",
+    }
+    pg.query.mockResolvedValue({ rows: [] }) 
+    pg.query.mockResolvedValueOnce({ rows: [PAGE2] }) 
+
+    await import('./index.js')
+
+    expect(pg.query).toHaveBeenCalledWith(expect.stringContaining('INSERT INTO products'), expect.anything())
+    expect(pg.query).toHaveBeenCalledWith(
+      expect.stringContaining('INSERT INTO sellers'),
+      expect.arrayContaining([OFFER.seller]),
+    )
+    expect(pg.query).toHaveBeenCalledWith(
+      expect.stringContaining('INSERT INTO offers'),
+      expect.arrayContaining([PAGE.site, PAGE.url, OFFER.price, OFFER.curency]),
+    )
   })
 })
--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -23,6 +23,13 @@ const offer = {
   url: page.url,
   site: page.site,
   product: product.id,
+  price: productTag.offers?.price,
+  currency: productTag.offers?.priceCurrency,
+  seller: productTag.offers?.seller.name,
+}
+
+if (offer.seller) {
+  await db.query('INSERT INTO sellers (name) VALUES ($1)', [offer.seller])
 }
 
 await db.query(
@@ -30,6 +37,6 @@ await db.query(
   [product.id, product.title, product.description, product.image],
 )
 await db.query(
-  'INSERT INTO offers (id, url, site, product) VALUES ($1, $2, $3, $4);',
-  [offer.id, offer.url, offer.site, offer.product],
+  'INSERT INTO offers (id, url, site, product, seller, price, currency) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+  [offer.id, offer.url, offer.site, offer.product, offer.seller, offer.price, offer.currency],
 )
