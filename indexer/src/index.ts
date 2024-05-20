366e338c5d4818e76cf33827737365e2e50ad416
Antonio Sánchez <sago.ant@gmail.com>
2024-05-20 19:59:55 +0000

    refactor: paralelized inserts

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -9,37 +9,30 @@ const page = (await db.query("SELECT * FROM pages"))[0]
 const { product, offer } = parseStructuredInfo(page)
 const augmented = await embedder.embed(product.title)
 
-if (offer.seller) {
-  await db.insertSeller(page.id, offer.seller)
-}
-
-if (product.brandName) {
-  await db.insertBrand(page.id, product.brandName, product.brandLogo)
-}
-
-await db.insertCategory(page.id, augmented.category)
-
-await db.insertTags(page.id, augmented.tags)
-
-await db.insertProduct(
-  page.id,
-  product.id,
-  product.title,
-  augmented.embedding,
-  augmented.category,
-  augmented.tags,
-  product.description,
-  product.image,
-  product.brandName,
-)
-
-await db.insertOffer(
-  page.id,
-  offer.id,
-  offer.url,
-  offer.site,
-  offer.product,
-  offer.seller,
-  offer.price,
-  offer.currency,
-)
+await Promise.all([
+  db.insertProduct(
+    page.id,
+    product.id,
+    product.title,
+    augmented.embedding,
+    augmented.category,
+    augmented.tags,
+    product.description,
+    product.image,
+    product.brandName,
+  ),
+  db.insertOffer(
+    page.id,
+    offer.id,
+    offer.url,
+    offer.site,
+    offer.product,
+    offer.seller,
+    offer.price,
+    offer.currency,
+  ),
+  db.insertCategory(page.id, augmented.category),
+  db.insertTags(page.id, augmented.tags),
+  offer.seller ? db.insertSeller(page.id, offer.seller) : Promise.resolve(),
+  product.brandName ? db.insertBrand(page.id, product.brandName, product.brandLogo) : Promise.resolve(),
+])
