a32e4b5ab2bcc15cbccd460d9b8762ad7b1cd856
Antonio Sánchez <sago.ant@gmail.com>
2024-05-21 07:07:33 +0000

    refactor: moved setup and teardown to after/beforeEach

--- indexer/__tests__/db.test.ts
+++ indexer/__tests__/db.test.ts
@@ -1,11 +1,14 @@
-import { expect, describe, it, beforeEach, vi } from 'vitest'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { Db } from '../src/Db'
 
 describe('DB', () => {
-  it('inserts traces', async () => {
-    const masterDb = Db()
+  let testDb: Db
+  let masterDb: Db
+  beforeEach(async () => {
+    masterDb = Db()
     await masterDb.query('CREATE DATABASE test;')
-    const testDb = Db('test')
+
+    testDb = Db('test')
     await testDb.query(`
       CREATE TABLE traces (
         id              uuid PRIMARY KEY,
@@ -15,7 +18,16 @@ describe('DB', () => {
         object_id       text
       );`
     )
+  })
 
+  afterEach(async () => {
+    await testDb.close()
+
+    await masterDb.query('DROP DATABASE test;')
+    await masterDb.close()
+  })
+
+  it('inserts traces', async () => {
     const TRACE = {
       pageId: "5f4f389d-cc00-4bdf-a484-278e3b18975c",
       objectTable: "sellers",
@@ -32,9 +44,5 @@ describe('DB', () => {
       object_id: TRACE.objectId,
       timestamp: expect.any(Date),
     }])
-
-    await testDb.close()
-    await masterDb.query('DROP DATABASE test;')
-    await masterDb.close()
   })
 })
