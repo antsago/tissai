b146e68b8327c9da8d2da0b701ad015fe12ecc2d
Antonio Sánchez <sago.ant@gmail.com>
2024-05-22 11:16:09 +0000

    refactor: return jsonLd from parsePage

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "node:crypto"
 import { Db, Page, PAGES } from "./Db/index.js"
 import Embedder from "./Embedder/index.js"
 import parsePage from "./parsePage.js"
@@ -6,7 +7,29 @@ const db = Db()
 const embedder = Embedder()
 const page = (await db.query<Page>(`SELECT * FROM ${PAGES}`))[0]
 
-const { product, offer } = parsePage(page)
+const { jsonLd } = parsePage(page)
+
+const productTag = jsonLd.filter((t) => t["@type"] === "Product")[0]
+const product = {
+  id: randomUUID(),
+  title: productTag.name,
+  description: productTag.description,
+  image: Array.isArray(productTag.image)
+    ? productTag.image
+    : [productTag.image],
+  brandName: productTag?.brand?.name,
+  brandLogo: productTag?.brand?.image,
+}
+const offer = {
+  id: randomUUID(),
+  url: page.url,
+  site: page.site,
+  product: product.id,
+  price: productTag.offers?.price,
+  currency: productTag.offers?.priceCurrency,
+  seller: productTag.offers?.seller.name,
+}
+
 const augmented = await embedder.embed(product.title)
 
 await Promise.all([
--- indexer/src/parsePage.ts
+++ indexer/src/parsePage.ts
@@ -1,39 +1,15 @@
-import { randomUUID } from "node:crypto"
 import { parse } from "node-html-parser"
 import { Page } from "./Db/index.js"
 
 function parsePage(page: Page) {
   const root = parse(page.body)
-  const productTag = root
+  const jsonLd = root
     .querySelectorAll('script[type="application/ld+json"]')
     .map((t) => t.textContent)
     .map((t) => JSON.parse(t))
-    .filter((t) => t["@type"] === "Product")[0]
-
-  const product = {
-    id: randomUUID(),
-    title: productTag.name,
-    description: productTag.description,
-    image: Array.isArray(productTag.image)
-      ? productTag.image
-      : [productTag.image],
-    brandName: productTag?.brand?.name,
-    brandLogo: productTag?.brand?.image,
-  }
-
-  const offer = {
-    id: randomUUID(),
-    url: page.url,
-    site: page.site,
-    product: product.id,
-    price: productTag.offers?.price,
-    currency: productTag.offers?.priceCurrency,
-    seller: productTag.offers?.seller.name,
-  }
 
   return {
-    product,
-    offer,
+    jsonLd,
   }
 }
 
