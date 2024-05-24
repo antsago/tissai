a64f32041085d844c04a4868d0f86626e1bd8029
Antonio Sánchez <sago.ant@gmail.com>
2024-05-24 06:22:47 +0000

    refactor: extracted extractEntities

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -3,43 +3,70 @@ import { Db, Page, PAGES } from "./Db/index.js"
 import Embedder from "./Embedder/index.js"
 import parsePage from "./parsePage.js"
 
+type StructuredData = ReturnType<typeof parsePage>
+async function extractEntities({ jsonLd }: StructuredData, embedder: Embedder, page: Page) {
+  const productTag = jsonLd.filter((t) => t["@type"] === "Product")[0]
+  const product = {
+    id: randomUUID(),
+    title: productTag.name,
+    description: productTag.description,
+    image: Array.isArray(productTag.image)
+      ? productTag.image
+      : [productTag.image],
+    brandName: productTag?.brand?.name,
+    brandLogo: productTag?.brand?.image,
+  }
+  const offer = {
+    id: randomUUID(),
+    url: page.url,
+    site: page.site,
+    product: product.id,
+    price: productTag.offers?.price,
+    currency: productTag.offers?.priceCurrency,
+    seller: productTag.offers?.seller.name,
+  }
+  const augmented = await embedder.embed(product.title)
+
+  const category = {
+    name: augmented.category,
+  }
+  const tags = augmented.tags.map(t => ({ name: t }))
+  const seller = offer.seller ? { name: offer.seller } : undefined
+  const brand = product.brandName ? { name: product.brandName, logo: product.brandLogo } : undefined
+
+  return {
+    category,
+    tags,
+    seller,
+    brand,
+    product: {
+      id: product.id,
+      title: product.title,
+      images: product.image,
+      description: product.description,
+      brand: brand?.name,
+      category: category.name,
+      tags: tags.map(t => t.name),
+      embedding: augmented.embedding,
+    },
+    offer,
+  }
+}
+
 const db = Db()
 const embedder = Embedder()
 const page = (await db.query<Page>(`SELECT * FROM ${PAGES}`))[0]
 
-const { jsonLd } = parsePage(page)
+const structuredData = parsePage(page)
 
-const productTag = jsonLd.filter((t) => t["@type"] === "Product")[0]
-const product = {
-  id: randomUUID(),
-  title: productTag.name,
-  description: productTag.description,
-  image: Array.isArray(productTag.image)
-    ? productTag.image
-    : [productTag.image],
-  brandName: productTag?.brand?.name,
-  brandLogo: productTag?.brand?.image,
-}
-const offer = {
-  id: randomUUID(),
-  url: page.url,
-  site: page.site,
-  product: product.id,
-  price: productTag.offers?.price,
-  currency: productTag.offers?.priceCurrency,
-  seller: productTag.offers?.seller.name,
-}
-
-const augmented = await embedder.embed(product.title)
+const { product, offer, category, tags, seller, brand } = await extractEntities(structuredData, embedder, page)
 
 await Promise.all(
   [
-    db.categories.create(page.id, augmented.category),
-    augmented.tags.map((name) => db.tags.create(page.id, name)),
-    offer.seller ? db.sellers.create(page.id, offer.seller) : Promise.resolve(),
-    product.brandName
-      ? db.brands.create(page.id, product.brandName, product.brandLogo)
-      : Promise.resolve(),
+    db.categories.create(page.id, category.name),
+    tags.map((tag) => db.tags.create(page.id, tag.name)),
+    seller ? db.sellers.create(page.id, seller.name) : Promise.resolve(),
+    brand ? db.brands.create(page.id, brand.name, brand.logo) : Promise.resolve(),
   ].flat(),
 )
 
@@ -47,12 +74,12 @@ await db.products.create(
   page.id,
   product.id,
   product.title,
-  augmented.embedding,
-  augmented.category,
-  augmented.tags,
+  product.embedding,
+  product.category,
+  product.tags,
   product.description,
-  product.image,
-  product.brandName,
+  product.images,
+  product.brand,
 )
 await db.offers.create(
   page.id,
