a707cc27ad5d22081fdec97dea84a0df5a93d625
Antonio Sánchez <sago.ant@gmail.com>
2024-05-24 08:38:55 +0000

    refactor: renamed variable

--- indexer/src/EntityExtractor/PythonPool.ts
+++ indexer/src/EntityExtractor/PythonPool.ts
@@ -5,19 +5,19 @@ type Resolver<T> = (value: T | PromiseLike<T>) => void
 function PythonPool<Input extends string | object, Output>(scriptPath: string) {
   const resolvers: Resolver<Output>[] = []
 
-  const model = new PythonShell(scriptPath, {
+  const worker = new PythonShell(scriptPath, {
     mode: "json",
     pythonOptions: ["-u"], // get print results in real-time
   })
 
-  model.on("message", (message: Output) => {
+  worker.on("message", (message: Output) => {
     resolvers.shift()?.(message)
   })
 
   return async (query: Input) => {
     const promise = new Promise<Output>((res) => resolvers.push(res))
 
-    model.send(query)
+    worker.send(query)
     return promise
   }
 }
