f65747270ab3e4b0b0d52b4a9fb3df2c16463e40
Antonio Sánchez <sago.ant@gmail.com>
2024-05-25 07:24:06 +0000

    feat: tag extraction tests

--- indexer/src/EntityExtractor/EntityExtractor.test.ts
+++ indexer/src/EntityExtractor/EntityExtractor.test.ts
@@ -2,33 +2,36 @@ import { expect, describe, it, beforeEach, vi } from "vitest"
 import { MockPython, AUGMENTED_DATA, PAGE } from "#mocks"
 import EntityExtractor from "./EntityExtractor.js"
 
+const productTag = {
+  "@context": ["https://schema.org/"],
+  "@type": ["Product"],
+  name: ["The name of the product"],
+  productID: ["121230"],
+  brand: [{
+    "@type": ["Brand"],
+    name: ["WEDZE"],
+    image: ["https://brand.com/image.jpg"],
+  }],
+  description: ["The description"],
+  image: ["https://example.com/image.jpg"],
+}
+const DERIVED_DATA = {
+  ...AUGMENTED_DATA,
+  embedding: JSON.parse(AUGMENTED_DATA.embedding),
+}
+
 describe("EntityExtractor", () => {
+  let extract
   let python: MockPython
   beforeEach(async () => {
     python = MockPython()
+    extract = EntityExtractor()
   })
 
   it("extracts category", async () => {
-    const productTag = {
-      "@context": ["https://schema.org/"],
-      "@type": ["Product"],
-      name: ["The name of the product"],
-      productID: ["121230"],
-      brand: [{
-        "@type": ["Brand"],
-        name: ["WEDZE"],
-        image: ["https://brand.com/image.jpg"],
-      }],
-      description: ["The description"],
-      image: ["https://example.com/image.jpg"],
-    }
-    const DERIVED_DATA = {
-      ...AUGMENTED_DATA,
-      embedding: JSON.parse(AUGMENTED_DATA.embedding),
-    }
     python.mockImplementation(() => DERIVED_DATA)
 
-    const { category } = await EntityExtractor()(
+    const { category } = await extract(
       { jsonLd: [productTag], opengraph: {}, headings: {} },
       PAGE,
     )
@@ -36,5 +39,36 @@ describe("EntityExtractor", () => {
     expect(category).toStrictEqual({
       name: DERIVED_DATA.category,
     })
+  })
+
+  it("extracts tags", async () => {
+    python.mockImplementation(() => DERIVED_DATA)
+
+    const { tags } = await extract(
+      { jsonLd: [productTag], opengraph: {}, headings: {} },
+      PAGE,
+    )
+
+    expect(tags).toStrictEqual([{
+      name: DERIVED_DATA.tags[0],
+    }])
+  })
+
+  it("extracts multiple tags", async () => {
+    const foundTags = ["two", "tags"]
+    python.mockImplementation(() => ({
+      ...DERIVED_DATA,
+      tags: foundTags,
+    }))
+
+    const { tags } = await extract(
+      { jsonLd: [productTag], opengraph: {}, headings: {} },
+      PAGE,
+    )
+
+    expect(tags).toStrictEqual([
+      { name: foundTags[0] },
+      { name: foundTags[1] },
+    ])
   })
 })
