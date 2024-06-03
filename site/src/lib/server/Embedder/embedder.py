04dfed5f7b2fb49eff2b520053090853cbd2ae6a
Antonio Sánchez <sago.ant@gmail.com>
2024-06-03 09:54:11 +0000

    refactor: unified black script in root

--- site/src/lib/server/Embedder/embedder.py
+++ site/src/lib/server/Embedder/embedder.py
@@ -2,12 +2,12 @@ import sys
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer("all-MiniLM-L6-v2")
 # Control printing of embedding
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
 
 for query in sys.stdin:
-  embedding = model.encode([query], convert_to_numpy=True)[0]
-  print(embedding)
+    embedding = model.encode([query], convert_to_numpy=True)[0]
+    print(embedding)
