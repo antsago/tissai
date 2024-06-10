bfd03e8d90d924ae6c0a4c46eaba28cfd262ebfc
Antonio Sánchez <sago.ant@gmail.com>
2024-06-10 13:45:03 +0000

    feat: styled imagecarousel

--- site/src/lib/components/Icon.svelte
+++ site/src/lib/components/Icon.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   let classes = ""
-  let title: string | undefined = undefined
-  export { classes as class, title }
+  export { classes as class }
+  export let solid = false
+  export let title: string | undefined = undefined
 </script>
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
-  fill="none"
   viewBox="0 0 24 24"
   stroke="currentColor"
+  fill={solid ? "currentColor": "none"}
   class={classes}
   role="img"
 >
--- site/src/routes/product/[productId]/ImageCarousel.svelte
+++ site/src/routes/product/[productId]/ImageCarousel.svelte
@@ -5,9 +5,11 @@
   export let alt:string
 </script>
 
-<ul class="flex flex-row overflow-x-scroll space-x-4 px-8 py-4">
+<ul class="flex flex-row overflow-x-scroll space-x-4 px-12 py-4">
   {#if !images || images.length === 0}
-    <MissingImage title="Sin imagenes" />
+    <li class="flex w-44 h-28 m-auto bg-stone-200/50 rounded border border-stone-300">
+      <MissingImage class="m-auto stroke-[0.5] text-stone-500/25 h-10" title="Sin imagenes" />
+    </li>
   {:else}
     {#each images as image}
       <li class="min-w-56 max-w-56">
