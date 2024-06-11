2a0c4cd9ce5498542372c3cf6be8941157b4ac65
Antonio Sánchez <sago.ant@gmail.com>
2024-06-11 07:38:31 +0000

    feat: styled image carousel below tablet

--- site/src/routes/product/[productId]/ImageCarousel.svelte
+++ site/src/routes/product/[productId]/ImageCarousel.svelte
@@ -5,25 +5,28 @@
   export let alt: string
 </script>
 
-<ul class="flex flex-row overflow-x-scroll space-x-4 px-12 py-4">
-  {#if !images || images.length === 0}
-    <li
-      class="flex w-44 h-28 m-auto bg-stone-200/50 rounded border border-stone-300"
-    >
-      <MissingImage
-        class="m-auto stroke-[0.5] text-stone-500/25 h-10"
-        title="Sin imagenes"
-      />
-    </li>
-  {:else}
-    {#each images as image}
-      <li class="min-w-56 max-w-56">
-        <img
-          class="rounded border border-stone-200/50 aspect-square object-cover"
-          src={image}
-          {alt}
+<div class="w-full overflow-x-scroll">
+  <ul class="flex flex-row space-x-4 px-12 py-4 w-max min-w-full justify-center">
+    <!-- class="sticky top-0 w-full max-w-sm md:max-w-md mx-auto md:rounded border border-stone-200/50 aspect-square object-cover" -->
+    {#if !images || images.length === 0}
+      <li
+        class="flex w-44 h-28 m-auto bg-stone-200/50 rounded border border-stone-300"
+      >
+        <MissingImage
+          class="m-auto stroke-[0.5] text-stone-500/25 h-10"
+          title="Sin imagenes"
         />
       </li>
-    {/each}
-  {/if}
-</ul>
+    {:else}
+      {#each images as image}
+        <li class="max-w-[calc(100vw-6rem)]">
+          <img
+            class="w-full max-h-96 rounded border border-stone-200/50 aspect-square object-cover"
+            src={image}
+            {alt}
+          />
+        </li>
+      {/each}
+    {/if}
+  </ul>
+</div>
