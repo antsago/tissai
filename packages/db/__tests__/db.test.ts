bf579d47ec160044e61f2b287bc78f5f15407aaa
Antonio Sánchez <sago.ant@gmail.com>
2024-06-14 07:28:50 +0000

    refactor: reorganized tests for reuse

--- packages/db/__tests__/db.test.ts
+++ packages/db/__tests__/db.test.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from "node:crypto"
-import { describe, test } from "vitest"
+import { describe, test, beforeEach } from "vitest"
 import {
   PRODUCT,
   SITE,
@@ -13,7 +13,8 @@ import {
 } from "#mocks"
 import { PRODUCTS } from "../src/index.js"
 
-const it = test.extend<{ db: dbFixture }>({
+type Fixtures = { db: dbFixture }
+const it = test.extend<Fixtures>({
   db: dbFixture,
 })
 
@@ -54,7 +55,7 @@ describe.concurrent("db", () => {
     ])
   })
 
-  it("searches products", async ({ expect, db }) => {
+  describe("search", () => {
     const baseProduct = {
       title: PRODUCT.title,
       images: PRODUCT.images,
@@ -72,69 +73,49 @@ describe.concurrent("db", () => {
       id: randomUUID(),
       embedding: [1, ...PRODUCT.embedding.slice(1)],
     }
-    await db.load({
-      categories: [CATEGORY],
-      tags: [TAG],
-      brands: [BRAND],
-      products: [product1, product2],
+    beforeEach<Fixtures>(async ({ db }) => {
+      await db.load({
+        categories: [CATEGORY],
+        tags: [TAG],
+        brands: [BRAND],
+        products: [product1, product2],
+      })
     })
 
-    const result = await db.searchProducts({ embedding: product2.embedding })
-
-    expect(result).toStrictEqual([
-      {
-        id: product2.id,
-        title: product2.title,
-        image: product2.images[0],
-        brand: undefined,
-      },
-      {
-        id: product1.id,
-        title: product1.title,
-        image: product1.images[0],
-        brand: BRAND,
-      },
-    ])
-  })
-
-  it("filters search by brand", async ({ expect, db }) => {
-    const baseProduct = {
-      title: PRODUCT.title,
-      images: PRODUCT.images,
-      category: PRODUCT.category,
-      tags: PRODUCT.tags,
-    }
-    const product1 = {
-      ...baseProduct,
-      id: randomUUID(),
-      embedding: [0, ...PRODUCT.embedding.slice(1)],
-      brand: PRODUCT.brand,
-    }
-    const product2 = {
-      ...baseProduct,
-      id: randomUUID(),
-      embedding: [1, ...PRODUCT.embedding.slice(1)],
-    }
-    await db.load({
-      categories: [CATEGORY],
-      tags: [TAG],
-      brands: [BRAND],
-      products: [product1, product2],
+    it("retrieves results", async ({ expect, db }) => {
+      const result = await db.searchProducts({ embedding: product2.embedding })
+  
+      expect(result).toStrictEqual([
+        {
+          id: product2.id,
+          title: product2.title,
+          image: product2.images[0],
+          brand: undefined,
+        },
+        {
+          id: product1.id,
+          title: product1.title,
+          image: product1.images[0],
+          brand: BRAND,
+        },
+      ])
     })
-
-    const result = await db.searchProducts({
-      embedding: product2.embedding,
-      brand: BRAND.name,
+  
+    it("filters by brand", async ({ expect, db }) => {
+      const result = await db.searchProducts({
+        embedding: product2.embedding,
+        brand: BRAND.name,
+      })
+  
+      expect(result).toStrictEqual([
+        {
+          id: product1.id,
+          title: product1.title,
+          image: product1.images[0],
+          brand: BRAND,
+        },
+      ])
     })
-
-    expect(result).toStrictEqual([
-      {
-        id: product1.id,
-        title: product1.title,
-        image: product1.images[0],
-        brand: BRAND,
-      },
-    ])
   })
 
   it("gets product details", async ({ expect, db }) => {
