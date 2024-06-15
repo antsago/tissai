ce76275d8878b000dbdd23e62d6e59fa94e91a3a
Antonio Sánchez <sago.ant@gmail.com>
2024-06-15 11:48:59 +0000

    fix: avoid typing errors

--- packages/site/src/routes/search/+page.server.ts
+++ packages/site/src/routes/search/+page.server.ts
@@ -5,16 +5,16 @@ export const load: PageServerLoad = async ({ url, locals }) => {
   const brand = url.searchParams.get("brand")
   const category = url.searchParams.get("category")
   const tags = url.searchParams.getAll("inc")
-  const min = parseFloat(url.searchParams.get("min"), 10)
-  const max = parseFloat(url.searchParams.get("max"), 10)
+  const min = url.searchParams.getAll("min").map(m => parseFloat(m))[0]
+  const max = url.searchParams.getAll("max").map(m => parseFloat(m))[0]
 
   return {
     products: await locals.products.search({
       query,
       brand,
       category,
-      min: isNaN(min) ? undefined : min,
-      max: isNaN(max) ? undefined : max,
+      min,
+      max,
       tags,
     }),
   }
