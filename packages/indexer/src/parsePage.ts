3a5477b9eb8e335cfa4c743a36c64a81e32cf8c6
Antonio Sánchez <sago.ant@gmail.com>
2024-06-26 10:12:45 +0000

    refactor: split parsePage into pipelines

--- packages/indexer/src/parsePage.ts
+++ packages/indexer/src/parsePage.ts
@@ -1,18 +1,21 @@
-import { parse } from "node-html-parser"
+import { HTMLElement, parse } from "node-html-parser"
 import he from "he"
 import type { Page } from "@tissai/db"
 
+type Headings = Partial<{
+  title: string
+  description: string
+  keywords: string
+  author: string
+  robots: string
+  canonical: string
+}>
+type OpenGraph = Record<`og:${string}`, string>
+
 export type StructuredData = {
   jsonLd: any[]
-  opengraph: Record<`og:${string}`, string>
-  headings: Partial<{
-    title: string
-    description: string
-    keywords: string
-    author: string
-    robots: string
-    canonical: string
-  }>
+  opengraph: OpenGraph
+  headings: Headings
 }
 
 function expandJsonLd(linkedData: any): any {
@@ -50,41 +53,52 @@ function expandJsonLd(linkedData: any): any {
   return Object.fromEntries(properties)
 }
 
-function parsePage(page: Page): StructuredData {
-  const root = parse(page.body)
-  const jsonLd = root
+function parsedPage(page: Page): HTMLElement {
+  return parse(page.body)
+}
+
+function jsonLd(parsedPage: HTMLElement): any[] {
+  return parsedPage
     .querySelectorAll('script[type="application/ld+json"]')
     .map((t) => t.rawText)
     .map((t) => JSON.parse(t))
     .map(expandJsonLd)
     .map((t) => (t["@graph"] ? t["@graph"] : t))
-    .flat()
+    .flat() 
+}
 
-  const headings = {
-    title: root?.querySelector("title")?.textContent,
-    description: root
-      ?.querySelector('meta[name="description"]')
+function headings(parsedPage: HTMLElement): Headings {
+  return {
+    title: parsedPage.querySelector("title")?.textContent,
+    description: parsedPage
+      .querySelector('meta[name="description"]')
       ?.getAttribute("content"),
-    keywords: root
-      ?.querySelector('meta[name="keywords"]')
+    keywords: parsedPage
+      .querySelector('meta[name="keywords"]')
       ?.getAttribute("content"),
-    author: root?.querySelector('meta[name="author"]')?.getAttribute("content"),
-    robots: root?.querySelector('meta[name="robots"]')?.getAttribute("content"),
-    canonical: root
-      ?.querySelector('link[rel="canonical"]')
+    author: parsedPage.querySelector('meta[name="author"]')?.getAttribute("content"),
+    robots: parsedPage.querySelector('meta[name="robots"]')?.getAttribute("content"),
+    canonical: parsedPage
+      .querySelector('link[rel="canonical"]')
       ?.getAttribute("href"),
   }
+}
 
-  const opengraph = Object.fromEntries(
-    root
+function opengraph(parsedPage: HTMLElement): OpenGraph {
+  return Object.fromEntries(
+    parsedPage
       .querySelectorAll('meta[property^="og:"]')
       .map((t) => [t.getAttribute("property"), t.getAttribute("content")]),
   )
+}
+
+function parsePage(page: Page): StructuredData {
+  const root = parsedPage(page)
 
   return {
-    jsonLd,
-    headings,
-    opengraph,
+    jsonLd: jsonLd(root),
+    headings: headings(root),
+    opengraph: opengraph(root),
   }
 }
 
