0457ea9e152525a5cf65199bf017a631f7796399
Antonio Sánchez <sago.ant@gmail.com>
2024-08-01 10:12:52 +0000

    feat: recovered schema and vocabulary building

--- packages/indexer/src/schemaExtractor/index.ts
+++ packages/indexer/src/schemaExtractor/index.ts
@@ -3,6 +3,42 @@ import { fileURLToPath } from "node:url"
 import { Db, query } from "@tissai/db"
 import { PythonPool } from "@tissai/python-pool"
 import matchLabels from "./matchLabels.js"
+import { type Schema, mergeSchemas, createSchema } from "./mergeSchemas.js"
+
+const SCHEMAS = {} as Record<string, Schema>
+
+type Vocabulary = {
+  canonical: string
+  synonyms: Record<string, number>
+}
+const VALUE_VOCABULARY = {} as Record<string, Vocabulary>
+
+const normalizeString = (str: string) =>
+  str
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")
+    .toLowerCase()
+const normalizeValue = (value: string) => {
+  const baseForm = normalizeString(value)
+
+  if (baseForm in VALUE_VOCABULARY) {
+    const vocab = VALUE_VOCABULARY[baseForm]
+    vocab.synonyms[value] =
+      value in vocab.synonyms ? vocab.synonyms[value] + 1 : 1
+
+    if (vocab.synonyms[value] > vocab.synonyms[vocab.canonical]) {
+      vocab.canonical = value
+    }
+  } else {
+    VALUE_VOCABULARY[baseForm] = {
+      canonical: value,
+      synonyms: { [value]: 1 },
+    }
+  }
+
+  return baseForm
+}
+
 
 type Mapping = Record<string, number>
 const TOKEN_LABEL_MAPPING = {} as Record<string, Mapping>
@@ -31,6 +67,24 @@ const products = await db.stream(
 let skippedProducts = 0
 
 for await (let { title, attributes } of products) {
+  // const normalized = attributes.map((a) => ({
+  //   id: a.id,
+  //   product: a.product,
+  //   label: normalizeString(a.label),
+  //   value: normalizeValue(a.value),
+  // }))
+  // const schema = createSchema(normalized)
+
+  // if (!schema) {
+  //   continue
+  // }
+
+  // const { categoria, ...newSchema } = schema
+  // const oldSchema = SCHEMAS[categoria]
+
+  // SCHEMAS[categoria] = !oldSchema
+  //   ? newSchema
+  //   : mergeSchemas(newSchema, oldSchema)
   try {
     const tokens = await python.send(title)
     const mappings = matchLabels(tokens, attributes)
