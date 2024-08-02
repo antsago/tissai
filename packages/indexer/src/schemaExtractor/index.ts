813f0d527f036b0617d8abd7adf509cc6798c3e5
Antonio Sánchez <sago.ant@gmail.com>
2024-08-02 14:39:37 +0000

    feat: merged token mapping with vocabulary building

--- packages/indexer/src/schemaExtractor/index.ts
+++ packages/indexer/src/schemaExtractor/index.ts
@@ -12,11 +12,11 @@ const VOCABULARY = {} as Record<string, Vocabulary>
 type Mapping = Record<string, number>
 const TOKEN_LABEL_MAPPING = {} as Record<string, Mapping>
 
-// const currentDirectory = dirname(fileURLToPath(import.meta.url))
-// const python: PythonPool<string, string[]> = PythonPool(
-//   `${currentDirectory}/tissaiTokenizer.py`,
-//   console,
-// )
+const currentDirectory = dirname(fileURLToPath(import.meta.url))
+const python: PythonPool<string, string[]> = PythonPool(
+  `${currentDirectory}/tissaiTokenizer.py`,
+  console,
+)
 
 const db = Db()
 
@@ -33,42 +33,42 @@ const products = await db.stream(
     .compile(),
 )
 
-// let skippedProducts = 0
-
+let skippedProducts = 0
 for await (let { title, attributes } of products) {
-  const normalized = normalize(attributes, VOCABULARY)
-  const schema = createSchema(normalized)
+  try {
+    const tokens = await python.send(title)
+    const mappings = matchLabels(tokens, attributes)
+    mappings.forEach(({ token, label }) => {
+      if (!(token in TOKEN_LABEL_MAPPING)) {
+        TOKEN_LABEL_MAPPING[token] = { [label]: 1 }
+      } else if (label in TOKEN_LABEL_MAPPING[token]) {
+        TOKEN_LABEL_MAPPING[token][label] += 1
+      } else {
+        TOKEN_LABEL_MAPPING[token][label] = 1
+      }
+    })
 
-  if (!schema) {
-    continue
+    const normalized = normalize(attributes, VOCABULARY)
+    const schema = createSchema(normalized)
+
+    if (!schema) {
+      throw new Error("Product withouth schema")
+    }
+
+    const { categoria, ...newSchema } = schema
+    const oldSchema = SCHEMAS[categoria]
+
+    SCHEMAS[categoria] = !oldSchema
+      ? newSchema
+      : mergeSchemas(newSchema, oldSchema)
+  } catch (err) {
+    skippedProducts += 1
   }
-
-  const { categoria, ...newSchema } = schema
-  const oldSchema = SCHEMAS[categoria]
-
-  SCHEMAS[categoria] = !oldSchema
-    ? newSchema
-    : mergeSchemas(newSchema, oldSchema)
-
-  // try {
-  //   const tokens = await python.send(title)
-  //   const mappings = matchLabels(tokens, attributes)
-  //   mappings.forEach(({ token, label }) => {
-  //     if (!(token in TOKEN_LABEL_MAPPING)) {
-  //       TOKEN_LABEL_MAPPING[token] = { [label]: 1 }
-  //     } else if (label in TOKEN_LABEL_MAPPING[token]) {
-  //       TOKEN_LABEL_MAPPING[token][label] += 1
-  //     } else {
-  //       TOKEN_LABEL_MAPPING[token][label] = 1
-  //     }
-  //   })
-  // } catch (err) {
-  //   skippedProducts += 1
-  // }
 }
 
-// console.log(JSON.stringify({ TOKEN_LABEL_MAPPING, skippedProducts }))
-console.log(JSON.stringify({ SCHEMAS, VOCABULARY }))
+console.log(
+  JSON.stringify({ TOKEN_LABEL_MAPPING, VOCABULARY, SCHEMAS, skippedProducts }),
+)
 
 await db.close()
-// await python.close()
+await python.close()
