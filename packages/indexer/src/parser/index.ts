b6f10fd18fa088fbaeb91fd9b6ff2ab015e189a8
Antonio Sánchez <sago.ant@gmail.com>
2024-08-09 11:30:23 +0000

    feat: use mapping to label tokens

--- packages/indexer/src/parser/index.ts
+++ packages/indexer/src/parser/index.ts
@@ -1,14 +1,23 @@
 import parseTokens from "./parserAnalyser.js"
+import mapping from './mapping.js'
+
+type SpacyTokens = { isMeaningful: boolean, text: string }
+
+const labeler = (tokens: SpacyTokens[]) => tokens.map(t => ({
+  ...t,
+  label: t.isMeaningful ? Object.keys(mapping[t.text])[0] : 'filler'
+}))
 
 const tokens = [
-  { label: "categoria", text: "pantalones" },
-  { label: "deporte", text: "esquí" },
-  { label: "filler", text: "y" },
-  { label: "deporte", text: "nieve" },
-  { label: "filler", text: "con" },
-  { label: "cremallera", text: "cremallera" },
+  { isMeaningful: true, text: "pantalones" },
+  { isMeaningful: true, text: "esquí" },
+  { isMeaningful: false, text: "y" },
+  { isMeaningful: true, text: "nieve" },
+  { isMeaningful: false, text: "con" },
+  { isMeaningful: true, text: "CREMALLERA" },
 ]
 
-const statements = parseTokens(tokens)
+const labeled = labeler(tokens)
+const attributes = parseTokens(labeled)
 
-console.dir(statements, { depth: null })
+console.dir(attributes, { depth: null })
