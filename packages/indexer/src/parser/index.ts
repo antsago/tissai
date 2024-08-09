3d2d2b2936134faf56227be05076785e6b655b9d
Antonio Sánchez <sago.ant@gmail.com>
2024-08-09 12:19:29 +0000

    refactor: format

--- packages/indexer/src/parser/index.ts
+++ packages/indexer/src/parser/index.ts
@@ -1,12 +1,13 @@
 import parseTokens from "./parserAnalyser.js"
-import mapping from './mapping.js'
+import mapping from "./mapping.js"
 
-type SpacyTokens = { isMeaningful: boolean, text: string }
+type SpacyTokens = { isMeaningful: boolean; text: string }
 
-const labeler = (tokens: SpacyTokens[]) => tokens.map(t => ({
-  ...t,
-  label: t.isMeaningful ? Object.keys(mapping[t.text])[0] : 'filler'
-}))
+const labeler = (tokens: SpacyTokens[]) =>
+  tokens.map((t) => ({
+    ...t,
+    label: t.isMeaningful ? Object.keys(mapping[t.text])[0] : "filler",
+  }))
 
 const tokens = [
   { isMeaningful: true, text: "pantalones" },
