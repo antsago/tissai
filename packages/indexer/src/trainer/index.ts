75f2a9b641f031c755fba03b69a770b7a106b50e
Antonio Sánchez <sago.ant@gmail.com>
2024-08-10 16:35:13 +0000

    feat: added reporter

--- packages/indexer/src/trainer/index.ts
+++ packages/indexer/src/trainer/index.ts
@@ -2,9 +2,12 @@ import { dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 import { Db, query } from "@tissai/db"
 import { PythonPool } from "@tissai/python-pool"
+import { reporter } from "../Reporter.js"
 import Lexer from "../lexer/index.js"
 
 const TOKEN_LABEL_MAPPING = {} as Record<string, Record<string, number>>
+
+reporter.progress("Initializing database and pools")
 
 const db = Db()
 const lexer = Lexer()
@@ -12,13 +15,24 @@ const currentDirectory = dirname(fileURLToPath(import.meta.url))
 const python = PythonPool<
   { title: string; words: string[] },
   { label: string; value: string }[]
->(`${currentDirectory}/labelWords.py`, console)
+>(`${currentDirectory}/labelWords.py`, reporter)
 
+const [{ count: productCount }] = await db.query(
+  query
+    .selectFrom("products")
+    .select(({ fn }) => fn.count("id").as("count"))
+    .compile(),
+)
 const products = await db.stream(
-  query.selectFrom("products").select("products.title").compile(),
+  query.selectFrom("products").select(["title", "id"]).compile(),
 )
 
-for await (let { title } of products) {
+let index = 1
+for await (let { id, title } of products) {
+  try {
+      reporter.progress(
+        `Processing product ${index}/${productCount}: ${id} (${title})`,
+      )
   const tokens = await lexer.tokenize(title)
 
   const words = tokens.filter((t) => t.isMeaningful).map((t) => t.originalText)
@@ -55,8 +69,15 @@ for await (let { title } of products) {
         [label!]: (TOKEN_LABEL_MAPPING[text]?.[label!] ?? 0) + 1,
       }
     })
+
+    index += 1
+  } catch (err) {
+    const message = err instanceof Error ? err.message : String(err)
+    reporter.error(`[${id} (${title})]: ${message}`)
+  }
 }
 
+reporter.succeed(`Processed ${productCount} products`)
 console.log(JSON.stringify(TOKEN_LABEL_MAPPING))
 
 await db.close()
