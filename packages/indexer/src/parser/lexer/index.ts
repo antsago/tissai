aa4ca5077ed9db6d746e59353bdb4995f620da0f
Antonio Sánchez <sago.ant@gmail.com>
2024-08-10 15:30:40 +0000

    refactor: cleanedup schemaExtractor into dummy trainer

--- packages/indexer/src/parser/lexer/index.ts
+++ packages/indexer/src/parser/lexer/index.ts
@@ -1,6 +1,5 @@
 import { Lexer } from "./Lexer.js"
 
-export { normalizeString, type Token } from "./normalizer.js"
-export { Scanner } from "./Scanner.js"
+export type { Token } from "./normalizer.js"
 
 export default Lexer
--- packages/indexer/src/schemaExtractor/index.ts
+++ packages/indexer/src/schemaExtractor/index.ts
@@ -1,102 +1,31 @@
 import { Db, query } from "@tissai/db"
-import { normalizeString, Scanner } from "../parser/lexer/index.js"
-import { tokenizeAttributes } from "./matchLabels.js"
-import { type Schema, mergeSchemas, createSchema } from "./mergeSchemas.js"
-import normalize, { type Vocabulary } from "./normalize.js"
+import Lexer from "../parser/lexer/index.js"
 
-const SCHEMAS = {} as Record<string, Schema>
-const VOCABULARY = {} as Record<string, Vocabulary>
-
-type LabelCount = Record<string, number>
-const TOKEN_LABEL_MAPPING = {} as Record<string, LabelCount>
-let MAPPINGS!: Record<string, Record<string, string[]>>
-
-function schemaToMapping(schema: Schema) {
-  const mapping = {} as Record<string, string[]>
-
-  Object.entries(schema).forEach(([label, values]) => {
-    values.forEach((value) => {
-      if (!mapping[value]) {
-        mapping[value] = [label]
-      } else if (!mapping[value].includes(label)) {
-        mapping[value] = [...mapping[value], label]
-      }
-    })
-  })
-
-  return mapping
-}
-
-const scanner = Scanner()
+const TOKEN_LABEL_MAPPING = {} as Record<string, Record<string, number>>
 
 const db = Db()
+const lexer = Lexer()
 
 const products = await db.stream(
   query
     .selectFrom("products")
-    .innerJoin("attributes", "attributes.product", "products.id")
-    .select(({ fn }) => [
-      "products.id",
-      "products.title",
-      fn.jsonAgg("attributes").as("attributes"),
-    ])
-    .groupBy(["products.id"])
+    .select("products.title")
     .compile(),
 )
 
-function mapTokens(attributes: ReturnType<typeof tokenizeAttributes>) {
-  attributes.forEach(({ tokens }) => {
-    tokens
-      .filter(({ label }) => !!label)
-      .forEach(({ label: rawLabel, text: rawText }) => {
-        const label = normalizeString(rawLabel!)
-        const text = normalizeString(rawText)
+for await (let { title } of products) {
+  const tokens = await lexer.tokenize(title)
 
-        TOKEN_LABEL_MAPPING[text] = {
-          ...(TOKEN_LABEL_MAPPING[text] ?? {}),
-          [label]: (TOKEN_LABEL_MAPPING[text]?.[label] ?? 0) + 1,
-        }
-      })
+  const labeled = tokens.map(t => ({...t, label: t.isMeaningful ? "label" : undefined }))
+  labeled.filter(t => !!t.label).forEach(({ label, text }) => {
+    TOKEN_LABEL_MAPPING[text] = {
+      ...(TOKEN_LABEL_MAPPING[text] ?? {}),
+      [label!]: (TOKEN_LABEL_MAPPING[text]?.[label!] ?? 0) + 1,
+    }
   })
 }
 
-let skippedProducts = 0
-for await (let { title, attributes } of products) {
-  try {
-    const tokens = await scanner.tokenize(title)
-
-    const tokenizedAttributes = tokenizeAttributes(tokens, attributes)
-    mapTokens(tokenizedAttributes)
-    const normalized = normalize(tokenizedAttributes, VOCABULARY)
-
-    const schema = createSchema(normalized)
-
-    if (!schema) {
-      throw new Error("Product without schema")
-    }
-
-    const { categoria, ...newSchema } = schema
-    SCHEMAS[categoria] = mergeSchemas(newSchema, SCHEMAS[categoria])
-    MAPPINGS = Object.fromEntries(
-      Object.entries(SCHEMAS).map(([categoria, schema]) => [
-        categoria,
-        schemaToMapping(schema),
-      ]),
-    )
-  } catch (err) {
-    skippedProducts += 1
-  }
-}
-
-console.log(
-  JSON.stringify({
-    MAPPINGS,
-    VOCABULARY,
-    TOKEN_LABEL_MAPPING,
-    SCHEMAS,
-    skippedProducts,
-  }),
-)
+console.log(JSON.stringify(TOKEN_LABEL_MAPPING))
 
 await db.close()
-await scanner.close()
+await lexer.close()
