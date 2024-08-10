f94ad1853c1e93ad5c971775627508c303e23998
Antonio Sánchez <sago.ant@gmail.com>
2024-08-10 16:18:06 +0000

    feat: label words with llm

--- packages/indexer/src/trainer/index.ts
+++ packages/indexer/src/trainer/index.ts
@@ -1,31 +1,64 @@
+import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
 import { Db, query } from "@tissai/db"
+import { PythonPool } from "@tissai/python-pool"
 import Lexer from "../lexer/index.js"
 
 const TOKEN_LABEL_MAPPING = {} as Record<string, Record<string, number>>
 
 const db = Db()
 const lexer = Lexer()
+const currentDirectory = dirname(fileURLToPath(import.meta.url))
+const python = PythonPool<
+  { title: string; words: string[] },
+  { label: string; value: string }[]
+>(`${currentDirectory}/labelWords.py`, console)
 
 const products = await db.stream(
-  query
-    .selectFrom("products")
-    .select("products.title")
-    .compile(),
+  query.selectFrom("products").select("products.title").compile(),
 )
 
 for await (let { title } of products) {
   const tokens = await lexer.tokenize(title)
 
-  const labeled = tokens.map(t => ({...t, label: t.isMeaningful ? "label" : undefined }))
-  labeled.filter(t => !!t.label).forEach(({ label, text }) => {
-    TOKEN_LABEL_MAPPING[text] = {
-      ...(TOKEN_LABEL_MAPPING[text] ?? {}),
-      [label!]: (TOKEN_LABEL_MAPPING[text]?.[label!] ?? 0) + 1,
+  const words = tokens.filter((t) => t.isMeaningful).map((t) => t.originalText)
+  const labels = await python.send({ title, words })
+
+  let labelsIndex = 0
+  const labeled = tokens.map((t) => {
+    if (!t.isMeaningful) {
+      return {
+        ...t,
+        label: undefined,
+      }
+    }
+
+    const label = labels[labelsIndex]
+    if (label.value !== t.originalText) {
+      throw new Error(
+        `With title "${title}", expected to match ${label.value} with ${t.originalText}`,
+      )
+    }
+    labelsIndex += 1
+
+    return {
+      ...t,
+      label: label.label,
     }
   })
+
+  labeled
+    .filter((t) => !!t.label)
+    .forEach(({ label, text }) => {
+      TOKEN_LABEL_MAPPING[text] = {
+        ...(TOKEN_LABEL_MAPPING[text] ?? {}),
+        [label!]: (TOKEN_LABEL_MAPPING[text]?.[label!] ?? 0) + 1,
+      }
+    })
 }
 
 console.log(JSON.stringify(TOKEN_LABEL_MAPPING))
 
 await db.close()
 await lexer.close()
+await python.close()
---
+++ packages/indexer/src/trainer/labelWords.py
@@ -0,0 +1,79 @@
+import sys
+import json
+from functools import reduce
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+modelName = "unsloth/tinyllama"
+tokenizer = AutoTokenizer.from_pretrained(modelName)
+model = AutoModelForCausalLM.from_pretrained(modelName)
+
+
+def getPrompt(title, labels):
+    labeled = "\n    ".join(
+        [f"""{token} <> {label}""" for (token, label) in labels]
+    )[:-1]
+    return f"""Dados los nombres de los productos siguientes, etiqueta cada palabra con el nombre de los atributos que representa:
+  Conjunto de cazadora y pantalón | Total look de hombre | SPF
+    Conjunto <> categoría
+    cazadora <> categoría
+    pantalón <> categoría
+    Total <> modelo
+    look <> modelo
+    hombre <> género,edad
+    SPF <> marca
+
+  Fred Perry Classic Swim Pantalones cortos
+    Fred <> marca
+    Perry <> marca
+    Classic <> modelo
+    Swim <> modelo
+    Pantalones <> categoría
+    cortos <> largo
+
+  PANTALÓN PITILLO CON CREMALLERA BALONCESTO ADULTO
+    PANTALÓN <> categoría
+    PITILLO <> talle
+    CREMALLERA <> cremallera
+    BALONCESTO <> deporte
+    ADULTO <> edad
+
+  Pantalón térmico interior de esqui y nieve Mujer SWIX Race
+    Pantalón <> categoría
+    térmico <> térmico
+    interior <> interior
+    esqui <> deporte
+    nieve <> deporte
+    Mujer <> género
+    SWIX <> marca
+    Race <> modelo
+
+  {title}
+    {labeled}"""
+
+
+def getLabel(title, labels, toLabel):
+    prompt = getPrompt(title, [*labels, (toLabel, "")])
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(
+        **inputs, max_new_tokens=100, stop_strings=["\n ", "\n\n"], tokenizer=tokenizer
+    )
+    output = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+    label = [
+        w for w in output[(len(prompt) + 1) :].split("\n") if w and not w.isspace()
+    ][-1]
+    return [*labels, (toLabel, label)]
+
+
+def labelWords(words, title):
+    labels = reduce(
+        lambda labels, wordToLabel: getLabel(title, labels, wordToLabel), words, []
+    )
+    return [{"label": label, "value": token} for (token, label) in labels]
+
+
+for rawQuery in sys.stdin:
+    query = json.loads(rawQuery)
+    response = labelWords(query["words"], query["title"])
+    print(json.dumps(response, separators=(",", ":")))
