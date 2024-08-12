7e46273d630b81f6865a97cf99587a9fb411e036
Antonio Sánchez <sago.ant@gmail.com>
2024-08-12 13:14:05 +0000

    refactor: simplified Label checks

--- packages/indexer/src/parser/parser/Label.ts
+++ packages/indexer/src/parser/parser/Label.ts
@@ -1,14 +1,13 @@
 import { MatchToken } from "./Filler.js"
 
-const Label = (desiredLabels?: string[]) => 
-  MatchToken((nextToken) => {
-    if (!nextToken.isMeaningful) {
-      return false
-    }
-
-    return !desiredLabels || desiredLabels.some((desiredLabel) =>
-      nextToken.labels.includes(desiredLabel),
-    )
-  })
+const Label = (desiredLabels?: string[]) =>
+  MatchToken(
+    (nextToken) =>
+      nextToken.isMeaningful &&
+      (!desiredLabels ||
+        desiredLabels.some((desiredLabel) =>
+          nextToken.labels.includes(desiredLabel),
+        )),
+  )
 
 export default Label
