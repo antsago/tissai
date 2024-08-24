548ca6b5b551ad99368f5ac62a110db158f9a91f
Antonio Sánchez <sago.ant@gmail.com>
2024-08-24 10:51:45 +0000

    refactor: added Compiler object to semantics

--- packages/indexer/src/parser/index.ts
+++ packages/indexer/src/parser/index.ts
@@ -2,52 +2,45 @@ import { Attributes } from "./grammar/index.js"
 import { TokenReader, type WordToken, type EntityToken } from "./TokenReader.js"
 import mapping from "./mapping.js"
 import Lexer, { type Token as LexerToken } from "../lexer/index.js"
+import { and, any, or, IsSymbol, IsString } from "./operators/index.js"
 import { Rule } from "./operators/Rule.js"
 
-const labeler = (tokens: LexerToken[]) =>
+type LabelMap = Record<string, Record<string, number>>
+const labeler = (map: LabelMap) => (tokens: LexerToken[]) =>
   tokens.map((t) => ({
     ...t,
-    labels: t.isMeaningful && t.text in mapping? Object.keys(mapping[t.text]) : ["filler"],
+    labels: t.isMeaningful && t.text in map? Object.keys(map[t.text]) : ["filler"],
   }))
 
-const compile = <Output>(check: Rule<WordToken, Output>) => async (title: string) => {
+function Compiler<Output>(map: LabelMap, grammar: Rule<WordToken, Output>) {
   const lexer = Lexer()
+  const label = labeler(map)
 
-  const tokens = await lexer.tokenize(title)
-  const labeled = labeler(tokens)
-  const match = await check(TokenReader(labeled))
+  const compile = async (title: string) => {
+    const tokens = await lexer.tokenize(title)
+    const labeled = label(tokens)
+    return grammar(TokenReader(labeled))
+  }
 
-  await lexer.close()
-  
-  return match
+  return {
+    compile,
+    close: () => lexer.close(),
+  }
 }
-
-// const attributes = await compile(Attributes)("Pantalones esquí y nieve con CREMALLERA")
-// console.dir(attributes, { depth: null })
-
-import { and, any, or, IsSymbol } from "./operators/index.js"
+type Compiler<Output> = ReturnType<typeof Compiler<Output>>
 
 const Equals = Symbol('Key-Value assignment')
 const ValueSeparator = Symbol('Value separator')
 const PropertyEnd = Symbol('Property end')
 
-const IsString = (token?: string) => (reader: TokenReader<EntityToken>) => {
-  const nextToken = reader.get()
-  if (nextToken && typeof nextToken !== 'symbol' && (token === undefined || nextToken === token)) {
-    reader.next()
-    return nextToken
-  }
-
-  return null
-}
-const deeplyParsed = <Output>(check: Rule<WordToken, Output>) => async (reader: TokenReader<EntityToken>) => {
+const parseAs = <Output>(compiler: Compiler<Output>) => async (reader: TokenReader<EntityToken>) => {
   const nextToken = reader.get()
   
   if (!nextToken || typeof nextToken === 'symbol') {
     return null
   }
 
-  const match = await compile(check)(nextToken)
+  const match = await compiler.compile(nextToken)
   if (match === null) {
     return null
   }
@@ -56,12 +49,14 @@ const deeplyParsed = <Output>(check: Rule<WordToken, Output>) => async (reader: 
   return match
 }
 
+const attributesCompiler = Compiler(mapping, Attributes)
 const EQ = IsSymbol(Equals)
 const VS = IsSymbol(ValueSeparator)
 const PE = IsSymbol(PropertyEnd)
 const ArrayValue = and(IsString(), any(and(VS, IsString())), PE)
 const Property = (key: string) => and(IsString(key), EQ, ArrayValue)
-const Title = and(IsString('name'), EQ, deeplyParsed(Attributes), PE)
+const TitleValue = parseAs(attributesCompiler)
+const Title = and(IsString('name'), EQ, TitleValue, PE)
 const Product = any(or(Title, Property('description'), Property("image")))
 
 const PRODUCT_SCHEMA = {
@@ -80,4 +75,5 @@ const ProductTokens = [
 ]
 const reader = TokenReader(ProductTokens)
 const result = await Product(reader)
+await attributesCompiler.close()
 console.dir(result, { depth: null })
