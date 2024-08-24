1856016cfcc4e3de07a9709ae4130ed84d53c12e
Antonio Sánchez <sago.ant@gmail.com>
2024-08-24 12:41:54 +0000

    feat: created rule operator

--- packages/indexer/src/parser/grammar/grammar.ts
+++ packages/indexer/src/parser/grammar/grammar.ts
@@ -1,7 +1,21 @@
-import type { EntityToken, WordToken } from "../types.js"
+import type { EntityToken, WordToken, Rule, RuleResult, RuleReader } from "../types.js"
 import type { Compiler } from "../Compiler.js"
 import { withL, and, any, or, Token, parseAs, type Context } from "../operators/index.js"
 import { Equals, ValueSeparator, PropertyEnd } from "./index.js"
+
+const rule = <R extends Rule<never, unknown>, O>(
+  check: R,
+  transform: (ruleOutput: NonNullable<Awaited<RuleResult<R>>>) => O,
+) =>
+  async (reader: RuleReader<R>) => {
+    const match = await (check(reader) as RuleResult<R>)
+
+    if (!match) {
+      return null
+    }
+
+    return transform(match)
+  }
 
 export const productGrammar = (compileGrammar: Compiler["compile"]) => {
   // Attributes Grammar
@@ -16,7 +30,6 @@ export const productGrammar = (compileGrammar: Compiler["compile"]) => {
   )
   const Attributes = any(or(Attribute, Filler))
 
-
   // ProductGrammar
   const IsString = (text?: string) =>
     Token(
@@ -29,12 +42,16 @@ export const productGrammar = (compileGrammar: Compiler["compile"]) => {
   const EQ = IsSymbol(Equals)
   const VS = IsSymbol(ValueSeparator)
   const PE = IsSymbol(PropertyEnd)
-  
-  const ArrayValue = and(IsString(), any(and(VS, IsString())), PE)
+
+  const ArrayValue = rule(
+    and(IsString(), any(and(VS, IsString())), PE),
+    (tokens) => tokens.flat(Infinity).filter(t => typeof t !== "symbol"),
+  )
+
   const Property = (key: string) => and(IsString(key), EQ, ArrayValue)
-  const TitleValue = parseAs(compileGrammar(Attributes))
-  const Title = and(IsString("name"), EQ, TitleValue, PE)
-  const Product = any(or(Title, Property("description"), Property("image")))
+  // const TitleValue = parseAs(compileGrammar(Attributes))
+  // const Title = and(IsString("name"), EQ, TitleValue, PE)
+  const Product = any(or(Property("name"), Property("description"), Property("image")))
 
   return Product
 }
