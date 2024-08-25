7b04e26e343ae7206fad4851952b65419aca8e9d
Antonio Sánchez <sago.ant@gmail.com>
2024-08-25 20:48:57 +0000

    feat: tokenize string key-value

--- packages/indexer/src/parser/index.ts
+++ packages/indexer/src/parser/index.ts
@@ -13,107 +13,123 @@ import {
   Id,
   Required,
 } from "./grammar/index.js"
+import { EntityToken } from "./types.js"
+import { expandEntry } from "../jsonLd.js"
 
-const ProductLd = {
-  "@context": "https://schema.org/",
-  "@type": "Product",
-  name: "The name of the product",
-  productID: "121230",
-  description: "The description",
-  image: "https://example.com/image.jpg",
-  brand: {
-    "@type": "Brand",
-    name: "WEDZE",
-    image: ["https://brand.com/image.jpg"],
-  },
+type LD = {
+  [key: string]: string
 }
 
-const TokenizedLd = [
+const tokenizeJson = (json: LD): EntityToken[] => [
   EntityStart,
-  "entity-0",
-  PropertyStart,
-  "@type",
-  Equals,
-  "Product",
-  PropertyEnd,
-  PropertyStart,
-  "name",
-  Equals,
-  "The name of the product",
-  PropertyEnd,
-  PropertyStart,
-  "description",
-  Equals,
-  "The description",
-  PropertyEnd,
-  PropertyStart,
-  "image",
-  Equals,
-  "https://example.com/image.jpg",
-  ValueSeparator,
-  "https://example.com/image2.jpg",
-  PropertyEnd,
-  PropertyStart,
-  "brand",
-  Equals,
-  Id,
-  "entity-1",
-  PropertyEnd,
+  "enitity-0",
+  Object.entries(json).map(([key, value]) => [PropertyStart, key, Equals, value, PropertyEnd]),
   EntityEnd,
-  EntityStart,
-  "entity-1",
-  PropertyStart,
-  "@type",
-  Equals,
-  "Brand",
-  PropertyEnd,
-  PropertyStart,
-  "name",
-  Equals,
-  "WEDZE",
-  PropertyEnd,
-  PropertyStart,
-  "image",
-  Equals,
-  "https://brand.com/image.jpg",
-  PropertyEnd,
-  EntityEnd,
-]
+].flat(2)
 
-const reader = TokenReader(TokenizedLd)
-const compiler = await Compiler(mapping)
+const ProductLd = {
+  // "@context": "https://schema.org/",
+  // "@type": "Product",
+  // name: "The name of the product",
+  productID: "121230",
+  // description: "The description",
+  image: "https://example.com/image.jpg",
+  // brand: {
+  //   "@type": "Brand",
+  //   name: "WEDZE",
+  //   image: ["https://brand.com/image.jpg"],
+  // },
+}
 
-const Product = Ontology([
-  {
-    [Required]: {
-      key: "@type",
-      value: "Product",
-    },
-    title: {
-      name: "name",
-      parse: {
-        as: "attributes",
-        with: compiler.compile(Attributes),
-      },
-    },
-    brand: {
-      name: "brand",
-      isReference: true,
-    },
-    description: "description",
-    images: "image",
-  },
-  {
-    [Required]: {
-      key: "@type",
-      value: "Brand",
-    },
-    name: "name",
-    icon: "image",
-  },
-])
+const tokens = tokenizeJson(ProductLd)
+console.dir(tokens, { depth: null })
 
-const result = await Product(reader)
+// const TokenizedLd = [
+//   EntityStart,
+//   "entity-0",
+//   PropertyStart,
+//   "@type",
+//   Equals,
+//   "Product",
+//   PropertyEnd,
+//   PropertyStart,
+//   "name",
+//   Equals,
+//   "The name of the product",
+//   PropertyEnd,
+//   PropertyStart,
+//   "description",
+//   Equals,
+//   "The description",
+//   PropertyEnd,
+//   PropertyStart,
+//   "image",
+//   Equals,
+//   "https://example.com/image.jpg",
+//   ValueSeparator,
+//   "https://example.com/image2.jpg",
+//   PropertyEnd,
+//   PropertyStart,
+//   "brand",
+//   Equals,
+//   Id,
+//   "entity-1",
+//   PropertyEnd,
+//   EntityEnd,
+//   EntityStart,
+//   "entity-1",
+//   PropertyStart,
+//   "@type",
+//   Equals,
+//   "Brand",
+//   PropertyEnd,
+//   PropertyStart,
+//   "name",
+//   Equals,
+//   "WEDZE",
+//   PropertyEnd,
+//   PropertyStart,
+//   "image",
+//   Equals,
+//   "https://brand.com/image.jpg",
+//   PropertyEnd,
+//   EntityEnd,
+// ]
 
-await compiler.close()
-console.dir(result, { depth: null })
+// const reader = TokenReader(TokenizedLd)
+// const compiler = await Compiler(mapping)
+
+// const Product = Ontology([
+//   {
+//     [Required]: {
+//       key: "@type",
+//       value: "Product",
+//     },
+//     title: {
+//       name: "name",
+//       parse: {
+//         as: "attributes",
+//         with: compiler.compile(Attributes),
+//       },
+//     },
+//     brand: {
+//       name: "brand",
+//       isReference: true,
+//     },
+//     description: "description",
+//     images: "image",
+//   },
+//   {
+//     [Required]: {
+//       key: "@type",
+//       value: "Brand",
+//     },
+//     name: "name",
+//     icon: "image",
+//   },
+// ])
+
+// const result = await Product(reader)
+
+// await compiler.close()
+// console.dir(result, { depth: null })
