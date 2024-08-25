3ad7a4dd36d7da679754867b8e92f99a7947aedb
Antonio Sánchez <sago.ant@gmail.com>
2024-08-25 15:48:33 +0000

    refactor: extracted Properties rule

--- packages/indexer/src/parser/grammar/entity.ts
+++ packages/indexer/src/parser/grammar/entity.ts
@@ -23,32 +23,39 @@ const definitions = (inputSchema: Schema): PropertyDefinition[] =>
     ...(typeof v === "string" ? { name: v } : v),
   }))
 
-type PropertyResult = { key?: string | symbol; value: any[] }
-const reduceProperties = (properties: (PropertyResult | PropertyResult[])[]) =>
-  Object.fromEntries(
-    properties
-      .flat()
-      .filter(({ key }) => !!key)
-      .map(({ key, value }) => [key, value.length === 1 ? value[0] : value]),
-  )
-
-export const Entity = (schema: Schema) => {
+const Properties = (schema: Schema) => {
   const requiredProperty = StringProperty({
     key: Required,
     name: schema[Required].key,
     value: schema[Required].value,
   })
   const definedProperties = definitions(schema).map(Property)
-  const properties = given(
-    requiredProperty,
-    any(or(...definedProperties, AnyProperty)),
-  )
 
   return restructure(
-    and(IsSymbol(EntityStart), IsString(), properties, IsSymbol(EntityEnd)),
+    given(requiredProperty, any(or(...definedProperties, AnyProperty))),
+    (properties) =>
+      Object.fromEntries(
+        properties
+          .flat()
+          .filter(({ key }) => !!key)
+          .map(({ key, value }) => [
+            key,
+            value.length === 1 ? value[0] : value,
+          ]),
+      ),
+  )
+}
+
+export const Entity = (schema: Schema) =>
+  restructure(
+    and(
+      IsSymbol(EntityStart),
+      IsString(),
+      Properties(schema),
+      IsSymbol(EntityEnd),
+    ),
     ([s, id, parsedProperties, e]) => ({
-      ...reduceProperties(parsedProperties),
+      ...parsedProperties,
       [Id]: id,
     }),
   )
-}
