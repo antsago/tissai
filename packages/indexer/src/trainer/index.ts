0ba298785be946d4142a24471919ce401c7e7092
Antonio Sánchez <sago.ant@gmail.com>
2024-08-29 10:56:01 +0000

    refactor: extracted labelTokens function

--- packages/indexer/src/trainer/index.ts
+++ packages/indexer/src/trainer/index.ts
@@ -2,9 +2,9 @@ import assert from 'node:assert/strict'
 import { Db, query } from "@tissai/db"
 import { PythonPool } from "@tissai/python-pool"
 import { reporter } from "../Reporter.js"
-import Lexer from "../lexer/index.js"
+import Lexer, { Token } from "../lexer/index.js"
 
-const TOKEN_LABEL_MAPPING = {} as Record<string, Record<string, number>>
+type Label = { label: string; value: string }
 
 reporter.progress("Initializing database and pools")
 
@@ -12,8 +12,37 @@ const db = Db()
 const lexer = Lexer()
 const python = PythonPool<
   { title: string; words: string[] },
-  { label: string; value: string }[]
+  Label[]
 >(`./labelWords.py`, reporter)
+
+const labelTokens = async (tokens: Token[], title: string) => {
+  const words = tokens
+    .filter((t) => t.isMeaningful)
+    .map((t) => t.originalText)
+  const labels = await python.send({ title, words })
+
+  let labelsIndex = 0
+  const labeled = tokens.map((t) => {
+    if (!t.isMeaningful) {
+      return {
+        ...t,
+        label: undefined,
+      }
+    }
+
+    const label = labels[labelsIndex]
+    assert.equal(label.value, t.originalText, `Expected "${label.value}" to equal "${t.originalText}" in "${title}"`)
+    
+    labelsIndex += 1
+
+    return {
+      ...t,
+      label: label.label,
+    }
+  })
+
+  return labeled
+}
 
 const [{ count: productCount }] = await db.query(
   query
@@ -22,9 +51,10 @@ const [{ count: productCount }] = await db.query(
     .compile(),
 )
 const products = await db.stream(
-  query.selectFrom("products").select(["title", "id"]).compile(),
+  query.selectFrom("products").select(["title", "id"]).limit(2).compile(),
 )
 
+const TOKEN_LABEL_MAPPING = {} as Record<string, Record<string, number>>
 let index = 1
 for await (let { id, title } of products) {
   try {
@@ -32,31 +62,7 @@ for await (let { id, title } of products) {
       `Processing product ${index}/${productCount}: ${id} (${title})`,
     )
     const tokens = await lexer.tokenize(title)
-
-    const words = tokens
-      .filter((t) => t.isMeaningful)
-      .map((t) => t.originalText)
-    const labels = await python.send({ title, words })
-
-    let labelsIndex = 0
-    const labeled = tokens.map((t) => {
-      if (!t.isMeaningful) {
-        return {
-          ...t,
-          label: undefined,
-        }
-      }
-
-      const label = labels[labelsIndex]
-      assert.equal(label.value, t.originalText, `Expected "${label.value}" to equal "${t.originalText}" for "${title}", `)
-      
-      labelsIndex += 1
-
-      return {
-        ...t,
-        label: label.label,
-      }
-    })
+    const labeled = await labelTokens(tokens, title)
 
     labeled
       .filter((t) => !!t.label)
