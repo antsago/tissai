a0724e2c8e6ac61bb212ad82c2f99a095658cfb8
Antonio Sánchez <sago.ant@gmail.com>
2024-08-30 07:38:51 +0000

    refactor: removed unused code

--- packages/indexer/src/parser/operators/unless.ts
+++ packages/indexer/src/parser/operators/unless.ts
@@ -10,23 +10,3 @@ export const unless =
 
     return match === null ? onMatch(reader) : null
   }
-
-export const given =
-  <T, CO, MO>(condition: Rule<T, CO>, onMatch: Rule<T, MO>) =>
-  async (reader: TokenReader<T>) => {
-    reader.savePosition()
-
-    while (reader.hasNext()) {
-      const match = await condition(reader)
-
-      if (match !== null) {
-        reader.restoreSave()
-        return onMatch(reader)
-      }
-
-      reader.next()
-    }
-
-    reader.restoreSave()
-    return null
-  }
