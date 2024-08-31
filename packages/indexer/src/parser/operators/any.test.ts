af278eef7667e32315bee531288d6b801cf6e675
Antonio Sánchez <sago.ant@gmail.com>
2024-08-31 09:18:42 +0000

    refactor: more consistent naming

--- packages/indexer/src/parser/operators/any.test.ts
+++ packages/indexer/src/parser/operators/any.test.ts
@@ -1,26 +1,26 @@
 import { expect, describe, it } from "vitest"
 import { TokenReader } from "../TokenReader.js"
 import { Token } from "./Token.js"
-import any from "./any.js"
+import rawAny from "./any.js"
 
 describe("any", () => {
-  const anyTrue = (tokens: boolean[]) => any(Token((t: boolean) => t))(TokenReader(tokens))
+  const any = (tokens: boolean[]) => rawAny(Token((t: boolean) => t))(TokenReader(tokens))
 
   it("accepts no matches", async () => {
     const tokens = [false]
-    const result = await anyTrue(tokens)
+    const result = await any(tokens)
     expect(result).toStrictEqual([])
   })
 
   it("accepts single match", async () => {
     const tokens = [true, false]
-    const result = await anyTrue(tokens)
+    const result = await any(tokens)
     expect(result).toStrictEqual([true])
   })
 
   it("accepts multiple matches", async () => {
     const tokens = [true, true, false]
-    const result = await anyTrue(tokens)
+    const result = await any(tokens)
     expect(result).toStrictEqual([true, true])
   })
 })
