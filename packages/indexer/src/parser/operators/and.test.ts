80962d598d401af7d17dae98f6298447d12727c9
Antonio Sánchez <sago.ant@gmail.com>
2024-08-31 10:19:18 +0000

    feat: and tests and position fix

---
+++ packages/indexer/src/parser/operators/and.test.ts
@@ -0,0 +1,37 @@
+import { expect, describe, it } from "vitest"
+import { TokenReader } from "../TokenReader.js"
+import { Token } from "./Token.js"
+import and from "./and.js"
+import { NonMatch } from "./nonMatch.js"
+
+describe("and", () => {
+  const IsYes = Token(t => t === "yes")
+  const IsNo = Token(t => t === "no")
+
+  it("requires first match", async () => {
+    const reader = TokenReader(["yes", "no"])
+
+    const result = await and(IsNo)(reader)
+
+    expect(result).toStrictEqual(NonMatch)
+    expect(reader.get()).toBe("yes")
+  })
+
+  it("requires subsequent rules", async () => {
+    const reader = TokenReader(["yes", "no"])
+
+    const result = await and(IsYes, IsYes)(reader)
+
+    expect(result).toStrictEqual(NonMatch)
+    expect(reader.get()).toBe("yes")
+  })
+
+  it("accepts all matches", async () => {
+    const reader = TokenReader(["yes", "no"])
+
+    const result = await and(IsYes, IsNo)(reader)
+
+    expect(result).toStrictEqual(["yes", "no"])
+    expect(reader.hasNext()).toBe(false)
+  })
+})
--- packages/indexer/src/parser/operators/and.ts
+++ packages/indexer/src/parser/operators/and.ts
@@ -8,17 +8,21 @@ type AndResults<T extends Rule<never, unknown>[]> = {
 const and =
   <I extends Rule<never, unknown>[]>(...checks: I) =>
   async (reader: RuleReader<I[number]>) => {
+    reader.savePosition()
+
     const result = [] as AndResults<I>
 
     for (const check of checks) {
       const match = await check(reader)
       if (match === NonMatch) {
+        reader.restoreSave()
         return NonMatch
       }
 
       result.push(match)
     }
 
+    reader.discardSave()
     return result
   }
 
