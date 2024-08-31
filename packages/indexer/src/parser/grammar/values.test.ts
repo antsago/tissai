bb07463cc49f83faecf5e62ce35aaca40c3e4211
Antonio Sánchez <sago.ant@gmail.com>
2024-08-31 12:13:07 +0000

    feat: isData tests

--- packages/indexer/src/parser/grammar/values.test.ts
+++ packages/indexer/src/parser/grammar/values.test.ts
@@ -2,24 +2,27 @@ import { expect, describe, it } from "vitest"
 import { Id, ValueSeparator } from "../../lexer/symbols.js"
 import { TokenReader } from "../TokenReader.js"
 import { NonMatch } from "../operators/nonMatch.js"
-import { Any, IsSymbol, IsValue } from "./values.js"
+import { Any, IsData, IsSymbol, IsValue } from "./values.js"
 
 describe("values", () => {
   describe("Any", () => {
     it.each([true, false, "", "asdf", Symbol(), 123])("matches '%s'", (token) => {
-      const result = Any(TokenReader([token]))
+      const reader = TokenReader([token])
+      const result = Any(reader)
       expect(result).toStrictEqual(token)
     })
   })
 
   describe("IsValue", () => {
     it.each([true, false, "", "asdf", 123, Id, ValueSeparator])("matches '%s'", (token) => {
-      const result = IsValue(TokenReader([token]))
+      const reader = TokenReader([token])
+      const result = IsValue(reader)
       expect(result).toStrictEqual(token)
     })
 
     it("rejects non-value symbols", () => {
-      const result = IsValue(TokenReader([Symbol()]))
+      const reader = TokenReader([Symbol()])
+      const result = IsValue(reader)
       expect(result).toStrictEqual(NonMatch)
     })
   })
@@ -27,17 +30,39 @@ describe("values", () => {
   describe("IsSymbol", () => {
     it("matches given symbol", () => {
       const symbol = Symbol("foo")
-
       const result = IsSymbol(symbol)(TokenReader([symbol]))
-
       expect(result).toStrictEqual(symbol)
     })
 
     it("rejectes other symbols", () => {
       const symbol = Symbol("foo")
-
       const result = IsSymbol(Symbol())(TokenReader([symbol]))
+      expect(result).toStrictEqual(NonMatch)
+    })
+  })
 
+  describe("IsData", () => {
+    it.each([true, false, "", "asdf", 123])("matches '%s'", (token) => {
+      const reader = TokenReader([token])
+      const result = IsData()(reader)
+      expect(result).toStrictEqual(token)
+    })
+
+    it("rejectes symbols", () => {
+      const reader = TokenReader([Symbol()])
+      const result = IsData()(reader)
+      expect(result).toStrictEqual(NonMatch)
+    })
+
+    it("matches expected data", () => {
+      const reader = TokenReader(["expected"])
+      const result = IsData("expected")(reader)
+      expect(result).toStrictEqual("expected")
+    })
+
+    it("rejects unexpected data", () => {
+      const reader = TokenReader(["unexpected"])
+      const result = IsData("expected")(reader)
       expect(result).toStrictEqual(NonMatch)
     })
   })
