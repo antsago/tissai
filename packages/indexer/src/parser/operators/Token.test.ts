1a916144a523b33d8ad9466935fd8b11cc532a37
Antonio Sánchez <sago.ant@gmail.com>
2024-08-31 09:08:51 +0000

    refactor: improved readibility

--- packages/indexer/src/parser/operators/Token.test.ts
+++ packages/indexer/src/parser/operators/Token.test.ts
@@ -1,30 +1,26 @@
 import { expect, describe, it } from "vitest"
 import { TokenReader } from "../TokenReader.js"
 import { NonMatch } from "./nonMatch.js"
-import { Token } from "./Token.js"
+import { Token as RawToken } from "./Token.js"
 
 describe("Token", () => {
+  const Token = <T>(check: (T) => boolean) => (tokens: T[]) => RawToken(check)(TokenReader(tokens))
+
   it("rejects no next token", () => {
     const tokens = []
-
-    const result = Token(() => true)(TokenReader(tokens))
-
+    const result = Token(() => true)(tokens)
     expect(result).toStrictEqual(NonMatch)
   })
 
   it("rejects tokens that fail the check", () => {
     const tokens = ["asdf"]
-
-    const result = Token(() => false)(TokenReader(tokens))
-
+    const result = Token(() => false)(tokens)
     expect(result).toStrictEqual(NonMatch)
   })
 
   it("accepts tokens that the passed check", () => {
     const tokens = ["asdf"]
-
-    const result = Token(() => true)(TokenReader(tokens))
-
+    const result = Token(() => true)(tokens)
     expect(result).toStrictEqual(tokens[0])
   })
 })
