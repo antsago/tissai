0d10cb7faee5f7562cfa90ac5b749c455e394f0a
Antonio Sánchez <sago.ant@gmail.com>
2024-08-31 12:06:21 +0000

    feat: isValue tests

--- packages/indexer/src/parser/grammar/values.test.ts
+++ packages/indexer/src/parser/grammar/values.test.ts
@@ -1,13 +1,26 @@
 import { expect, describe, it } from "vitest"
+import { Id, ValueSeparator } from "../../lexer/symbols.js"
 import { TokenReader } from "../TokenReader.js"
 import { NonMatch } from "../operators/nonMatch.js"
-import { Any, IsSymbol } from "./values.js"
+import { Any, IsSymbol, IsValue } from "./values.js"
 
 describe("values", () => {
   describe("Any", () => {
     it.each([true, false, "", "asdf", Symbol(), 123])("matches '%s'", (token) => {
       const result = Any(TokenReader([token]))
       expect(result).toStrictEqual(token)
+    })
+  })
+
+  describe("IsValue", () => {
+    it.each([true, false, "", "asdf", 123, Id, ValueSeparator])("matches '%s'", (token) => {
+      const result = IsValue(TokenReader([token]))
+      expect(result).toStrictEqual(token)
+    })
+
+    it("rejects non-value symbols", () => {
+      const result = IsValue(TokenReader([Symbol()]))
+      expect(result).toStrictEqual(NonMatch)
     })
   })
 
