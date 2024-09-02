f8f4a9ca9a0008180670ca828519cd8ff137bbd0
Antonio Sánchez <sago.ant@gmail.com>
2024-09-02 19:00:41 +0000

    feat: added missing schemas

--- packages/indexer/src/labeler/schemas.ts
+++ packages/indexer/src/labeler/schemas.ts
@@ -8,35 +8,64 @@ import {
 import * as model from "./model.js"
 import { getLabels } from "./getLabels.js"
 
-export const getSchemas = (lexer: Lexer): Schema[] => [
-  {
-    [Required]: {
-      key: "@type",
-      value: "Product",
-    },
-    title: {
-      name: "name",
-      parse: {
-        as: "attributes",
-        with: async (title: string) => {
-          const tokens = await lexer.fromText(title, getLabels(model))
-          return Attributes(TokenReader(tokens))
-        },
+const ProductSchema = (lexer: Lexer) => ({
+  [Required]: {
+    key: "@type",
+    value: "Product",
+  },
+  title: {
+    name: "name",
+    parse: {
+      as: "attributes",
+      with: async (title: string) => {
+        const tokens = await lexer.fromText(title, getLabels(model))
+        return Attributes(TokenReader(tokens))
       },
     },
-    brand: {
-      name: "brand",
-      isReference: true,
-    },
-    description: "description",
-    images: "image",
   },
-  {
-    [Required]: {
-      key: "@type",
-      value: "Brand",
-    },
-    name: "name",
-    icon: "image",
+  brand: {
+    name: "brand",
+    isReference: true,
   },
+  description: "description",
+  images: "image",
+  offers: {
+    name: "offers",
+    isReference: true,
+  }
+})
+
+const BrandSchema ={
+  [Required]: {
+    key: "@type",
+    value: "Brand",
+  },
+  name: "name",
+  logo: "image",
+}
+const OfferSchema = {
+  [Required]: {
+    key: "@type",
+    value: "Offer",
+  },
+  seller: {
+    name: "seller",
+    isReference: true,
+  },
+  price: "price",
+  currency: "priceCurrency",
+}
+const OrganizationSchema = {
+  [Required]: {
+    key: "@type",
+    value: "Organization",
+  },
+  name: "name"
+}
+
+export const getSchemas = (lexer: Lexer): Schema[] => [
+  ProductSchema(lexer),
+  BrandSchema,
+  OfferSchema,
+  OrganizationSchema,
 ]
