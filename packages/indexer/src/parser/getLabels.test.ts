eb20d9b9970111bdd099312e6bfd86195bac6b3d
Antonio Sánchez <sago.ant@gmail.com>
2024-09-02 09:02:11 +0000

    refactor: pass entire model to getLabels

--- packages/indexer/src/parser/getLabels.test.ts
+++ packages/indexer/src/parser/getLabels.test.ts
@@ -8,18 +8,18 @@ describe("getLabels", () => {
     isMeaningful: true,
     trailing: "",
   }
-  const schemas = {}
+  const SCHEMAS = {}
 
   it("returns 'unknown' for unknown words", () => {
     const vocabulary = {}
-    const result = getLabels({ schemas, vocabulary })([WORD_TOKEN])
+    const result = getLabels({ schemas: SCHEMAS, vocabulary })([WORD_TOKEN])
 
     expect(result).toStrictEqual(["unknown"])
   })
 
   it("returns most frequent label", () => {
     const vocabulary = { [WORD_TOKEN.text]: { unfrequent: 1, frequent: 10 } }
-    const result = getLabels({ schemas, vocabulary })([WORD_TOKEN])
+    const result = getLabels({ schemas: SCHEMAS, vocabulary })([WORD_TOKEN])
 
     expect(result).toStrictEqual(["frequent"])
   })
@@ -30,7 +30,31 @@ describe("getLabels", () => {
       word1: { categoría: 4, label: 2 },
       word2: { categoría: 5 },
     }
-    const result = getLabels({ schemas, vocabulary })([
+    const result = getLabels({ schemas: SCHEMAS, vocabulary })([
+      {
+        ...WORD_TOKEN,
+        text: "word1",
+      },
+      {
+        ...WORD_TOKEN,
+        text: "theCategory",
+      },
+      {
+        ...WORD_TOKEN,
+        text: "word2",
+      },
+    ])
+
+    expect(result).toStrictEqual(["label", "categoría", "unknown"])
+  })
+
+  it("update label probabilities with schema", () => {
+    const vocabulary = {
+      theCategory: { categoría: 10 },
+      word1: { categoría: 4, label: 2 },
+      word2: { categoría: 5 },
+    }
+    const result = getLabels({ schemas: SCHEMAS, vocabulary })([
       {
         ...WORD_TOKEN,
         text: "word1",
