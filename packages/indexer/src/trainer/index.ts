b2cdbb30da2af8b495184d69c5c7a7b526c30a30
Antonio Sánchez <sago.ant@gmail.com>
2024-09-02 14:47:24 +0000

    refactor: extracted runForAllPages

--- packages/indexer/src/trainer/index.ts
+++ packages/indexer/src/trainer/index.ts
@@ -4,50 +4,29 @@ import { reporter } from "../Reporter.js"
 import { Compiler, type Model, NonMatch } from "../parser/index.js"
 import { type Label, getSchemas } from "./schemas.js"
 import { updateModel } from "./updateModel.js"
+import { runForAllPages } from "./runForAllPages.js"
 
 reporter.progress("Initializing database and pools")
-
-const db = Db()
-const python = PythonPool<{ title: string; words: string[] }, Label[]>(
-  `./labelWords.py`,
-  console,
-)
-
-const compiler = Compiler(getSchemas(python))
-
-const [{ count: pageCount }] = await db.query(
-  query
-    .selectFrom("pages")
-    .select(({ fn }) => fn.count("id").as("count"))
-    .compile(),
-)
-const pages = await db.stream(
-  query.selectFrom("pages").select(["body", "url", "id"]).limit(1).compile(),
-)
 
 const MODEL: Model = {
   vocabulary: {},
   schemas: {},
 }
-let index = 1
-for await (let { id, body, url } of pages) {
-  try {
-    reporter.progress(`Processing page ${index}/${pageCount}: ${id} (${url})`)
+const db = Db()
+const python = PythonPool<{ title: string; words: string[] }, Label[]>(
+  `./labelWords.py`,
+  reporter,
+)
+const compiler = Compiler(getSchemas(python))
 
-    const entities = await compiler.parse(body)
+runForAllPages(db, async (page) => {
+  const entities = await compiler.parse(page.body)
 
-    if (entities !== NonMatch) {
-      updateModel(entities, MODEL)
-    }
-
-    index += 1
-  } catch (err) {
-    const message = err instanceof Error ? err.message : String(err)
-    reporter.error(`[${id} (${url})]: ${message}`)
+  if (entities !== NonMatch) {
+    updateModel(entities, MODEL)
   }
-}
+})
 
-reporter.succeed(`Processed ${pageCount} pages`)
 console.log(JSON.stringify(MODEL))
 
 await db.close()
---
+++ packages/indexer/src/trainer/runForAllPages.ts
@@ -0,0 +1,32 @@
+import { type Page, Db, query } from "@tissai/db"
+import { reporter } from "../Reporter.js"
+
+export const runForAllPages = async (db: Db, onPage: (page: Page) => Promise<void>) => {
+  reporter.progress("Setting up page stream")
+
+  const [{ count: totalPageCount }] = await db.query(
+    query
+      .selectFrom("pages")
+      .select(({ fn }) => fn.count("id").as("count"))
+      .compile(),
+  )
+  const pages = db.stream<Page>(query.selectFrom("pages").selectAll().compile())
+
+  let index = 1
+  for await (let page of pages) {
+    try {
+      reporter.progress(
+        `Processing page ${index}/${totalPageCount}: ${page.id} (${page.url})`,
+      )
+
+      await onPage(page)
+
+      index += 1
+    } catch (err) {
+      const message = err instanceof Error ? err.message : String(err)
+      reporter.error(`[${page.id} (${page.url})]: ${message}`)
+    }
+  }
+
+  reporter.succeed(`Processed ${totalPageCount} pages`)
+}
