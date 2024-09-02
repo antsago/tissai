0b5b192cd60496779ecbd401023b0379766358e3
Antonio Sánchez <sago.ant@gmail.com>
2024-09-02 07:36:30 +0000

    feat: single category label

--- packages/indexer/src/parser/getLabels.test.ts
+++ packages/indexer/src/parser/getLabels.test.ts
@@ -2,15 +2,48 @@ import { expect, describe, it } from "vitest"
 import { getLabels } from "./getLabels.js"
 
 describe("getLabels", () => {
+  const WORD_TOKEN = {
+    text: "word",
+    originalText: "word",
+    isMeaningful: true,
+    trailing: ""
+  }
+
+  it("returns 'unknown' for unknown words", () => {
+    const map = {}
+    const result = getLabels(map)([WORD_TOKEN])
+
+    expect(result).toStrictEqual(["unknown"])
+  })
+
   it("returns most frequent label", () => {
-    const map = { word: { unfrequent: 1, frequent: 10 }}
-    const result = getLabels(map)([{
-      text: "word",
-      originalText: "word",
-      isMeaningful: true,
-      trailing: ""
-    }])
+    const map = { [WORD_TOKEN.text]: { unfrequent: 1, frequent: 10 }}
+    const result = getLabels(map)([WORD_TOKEN])
 
     expect(result).toStrictEqual(["frequent"])
+  })
+
+  it("returns single category label", () => {
+    const map = {
+      theCategory: { categoría: 10 },
+      word1: { categoría: 4, label: 2 },
+      word2: { categoría: 5 },
+    }
+    const result = getLabels(map)([
+      {
+        ...WORD_TOKEN,
+        text: "word1",
+      },
+      {
+        ...WORD_TOKEN,
+        text: "theCategory",
+      },
+      {
+        ...WORD_TOKEN,
+        text: "word2",
+      },
+    ])
+
+    expect(result).toStrictEqual(["categoría", "unknown", "unknown"])
   })
 })
--- packages/indexer/src/parser/getLabels.ts
+++ packages/indexer/src/parser/getLabels.ts
@@ -2,7 +2,26 @@ import { type Token as LexerToken } from "../lexer/index.js"
 import { type LabelMap } from "./types.js"
 
 export const getLabels = (map: LabelMap) => (tokens: LexerToken[]) => {
-  const getLabel = (word: string) => Object.entries(map[word]).toSorted(([, a], [, b]) => b-a)[0][0]
+  let foundCategory = false
 
-  return tokens.map((t) => (t.text in map ? getLabel(t.text) : "unknown"))
+  const getLabel = (word: string) => {
+    if (!(word in map)) {
+      return undefined
+    }
+
+    const candidates = Object.entries(map[word]).toSorted(([, a], [, b]) => b-a).map(([label]) => label)
+    const label = candidates[0]
+    if (label !== "categoría") {
+      return label
+    }
+
+    if (!foundCategory) {
+      foundCategory = true
+      return label
+    }
+
+    return candidates[1]
+  }
+
+  return tokens.map((t) => getLabel(t.text) ?? "unknown")
 }
