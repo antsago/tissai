c4f994fc2799fc9c9e5859be22a7db7f1f59b9a1
Antonio Sánchez <sago.ant@gmail.com>
2024-09-13 06:42:53 +0000

    refactor: inject labels into labelTokens

--- packages/indexer/src/parser/lexer/Lexer.ts
+++ packages/indexer/src/parser/lexer/Lexer.ts
@@ -1,15 +1,19 @@
 import { Scanner } from "./Scanner.js"
-import { normalizer } from "./normalizer.js"
-import { type Labeler, labelTokens } from "./labelTokens.js"
+import { normalizer, type Token } from "./normalizer.js"
+import { labelTokens } from "./labelTokens.js"
 import { parsePage } from "./parsePage.js"
+
+type Labeler = (tokens: Token[]) => Promise<string[]> | string[]
 
 export function Lexer() {
   const scanner = Scanner()
 
   return {
     fromText: async (title: string, getLabels: Labeler) => {
-      const tokens = await scanner.tokenize(title)
-      return labelTokens(normalizer(tokens), getLabels)
+      const rawTokens = await scanner.tokenize(title)
+      const tokens = normalizer(rawTokens)
+      const labels = await getLabels(tokens.filter((t) => t.isMeaningful))
+      return labelTokens(tokens, labels)
     },
     fromPage: parsePage,
     close: () => scanner.close(),
--- packages/indexer/src/parser/lexer/labelTokens.ts
+++ packages/indexer/src/parser/lexer/labelTokens.ts
@@ -1,10 +1,6 @@
 import { Token } from "./normalizer.js"
 
-export type Labeler = (tokens: Token[]) => Promise<string[]> | string[]
-
-export const labelTokens = async (tokens: Token[], getLabels: Labeler) => {
-  const labels = await getLabels(tokens.filter((t) => t.isMeaningful))
-
+export const labelTokens = async (tokens: Token[], labels: string[]) => {
   let labelsIndex = 0
   const labeled = tokens.map((t) => {
     if (!t.isMeaningful) {
