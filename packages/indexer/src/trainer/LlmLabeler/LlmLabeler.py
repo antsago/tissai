6dd57376004c98d0d855f9c290a23195c1c50184
Antonio Sánchez <sago.ant@gmail.com>
2024-09-13 16:07:46 +0000

    fix: python handles empty predictions

--- packages/indexer/src/trainer/LlmLabeler/LlmLabeler.py
+++ packages/indexer/src/trainer/LlmLabeler/LlmLabeler.py
@@ -6,7 +6,7 @@ from getCategory import getCategory
 
 modelName = "unsloth/tinyllama"
 revision = "277240b3a621a06cfc02acf7b1f53bdbcb27843b"
-generator = pipeline(model=modelName, revision=revision)
+generator = pipeline(model=modelName, revision=revision, task="text-generation")
 
 
 for rawQuery in sys.stdin:
--- packages/indexer/src/trainer/LlmLabeler/utils.py
+++ packages/indexer/src/trainer/LlmLabeler/utils.py
@@ -4,7 +4,8 @@ from functools import reduce
 
 def getFirstWord(generatedText):
     firstSentence = generatedText.split("\n")[0]
-    firstWord = [w for w in firstSentence.split(" ") if w and not w.isspace()][0]
+    words = [w for w in firstSentence.split(" ") if w and not w.isspace()]
+    firstWord = words[0] if len(words) > 0 else ""
     return reduce(
         lambda word, punctuation: word.replace(punctuation, ""),
         string.punctuation,
