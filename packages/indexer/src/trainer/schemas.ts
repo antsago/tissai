140cbfff9862adfb115455cb52dc1375b3a1b698
Antonio Sánchez <sago.ant@gmail.com>
2024-09-13 20:20:43 +0000

    refactor: extracted getCategory

--- packages/indexer/src/trainer/schemas.ts
+++ packages/indexer/src/trainer/schemas.ts
@@ -9,26 +9,9 @@ import type { Reporter } from "../PageServer/index.js"
 import { LLM, Property } from "./LlmLabeler/index.js"
 import { extractSchemas } from "./extractSchemas.js"
 import { getFirstWord } from "./getFirstWord.js"
+import { getCategory } from "./getCategory.js"
 
 export const ProductType = Symbol("Product")
-
-const getCategory = async (llm: LLM, title: string) => {
-  const output = await llm.send({
-    prompt: `El producto con el título "PANTALÓN TÉRMICO INTERIOR DE ESQUI Y NIEVE MUJER SWIX RACE" es un pantalón.
-El producto con el título "CHÁNDAL CON SUDADERA CON CAPUCHA Y JOGGERS DE CORTE SLIM CON LAZO DE ASOS DESIGN" es un chándal.
-El producto con el título "JEAN 501® LEVI'S® ORIGINAL" es un vaquero.
-El producto con el título "BOTINES VAQUEROS" es un botín.
-El producto con el título "BOSTON CELTICS SHOWTIME CITY EDITION PANTALÓN NIKE DRI-FIT DE LA NBA - HOMBRE" es un pantalón.
-El producto con el título "${title}" es un`,
-    options: {
-      do_sample: false,
-      max_new_tokens: 5,
-      return_full_text: false,
-    },
-  })
-
-  return getFirstWord(output[0])
-}
 
 const getPrompt = (title: string, properties: Property[]) => {
   const labeled = properties.map(p => `${p.value} <> ${p.labels[0]}`).join("\n    ").slice(0, -1)
