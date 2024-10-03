bfc9022e26820090d209223fa38277f76ad8a1c3
Antonio Sánchez <sago.ant@gmail.com>
2024-10-03 08:18:02 +0000

    refactor: format

--- apps/site/src/routes/search/SuggestionTile.svelte
+++ apps/site/src/routes/search/SuggestionTile.svelte
@@ -6,9 +6,7 @@
   export let baseUrl: string
 </script>
 
-<div
-  class="w-full h-full flex flex-col rounded bg-stone-200"
->
+<div class="w-full h-full flex flex-col rounded bg-stone-200">
   <div class="relative w-full flex-basis-6 min-h-20 max-h-[25rem] rounded-t">
     <ChipContainer class="bg-stone-200">
       {#each suggestion.values as value}
@@ -17,14 +15,16 @@
             suggestion.label,
           )}={encodeURIComponent(value)}"
         >
-        <Chip background="bg-stone-100" orange>
-          {value}
-        </Chip>
+          <Chip background="bg-stone-100" orange>
+            {value}
+          </Chip>
         </a>
       {/each}
     </ChipContainer>
   </div>
   <h3
     class="block w-full px-4 py-3 border rounded-b border-stone-300/50 text-base text-center truncate text-stone-700"
-  >Filtrar por {suggestion.label}</h3>
+  >
+    Filtrar por {suggestion.label}
+  </h3>
 </div>
