314ac864239008b6f7ca763da8a4d2a2934896b9
Antonio Sánchez <sago.ant@gmail.com>
2024-10-08 10:29:39 +0000

    feat: handle hidden labels

--- apps/indexer/src/labeler/interpretations/interpretations.test.ts
+++ apps/indexer/src/labeler/interpretations/interpretations.test.ts
@@ -6,6 +6,29 @@ describe("createInterpretations", () => {
   it("returns interpretations", async () => {
     const nodeTree = {
       id: "2b3a9822-a8bd-4b13-9393-6640ce7bade3",
+      tally: 2,
+      children: [{
+        id: "2f311f14-b613-4a0d-ba84-5094d06cf3b6",
+        tally: 1,
+        children: null,
+      }],
+    }
+
+    const result = await createInterpretations(words, [nodeTree])
+
+    expect(result).toStrictEqual([
+      {
+        category: nodeTree.id,
+        attributes: [],
+        score: 1,
+        probability: 0.5,
+      },
+    ])
+  })
+
+  it("handles categories without labels", async () => {
+    const nodeTree = {
+      id: "2b3a9822-a8bd-4b13-9393-6640ce7bade3",
       tally: 1,
       children: null,
     }
--- apps/indexer/src/labeler/interpretations/interpretations.ts
+++ apps/indexer/src/labeler/interpretations/interpretations.ts
@@ -1,9 +1,12 @@
 import type { MatchedNodes } from "@tissai/db"
 
 export function createInterpretations(word: string[], nodes: MatchedNodes) {
+  const labelTally = nodes[0].children?.[0].tally ?? false
+  const categoryTally = nodes[0].tally
+
   return [
     {
-      probability: 1,
+      probability: labelTally === false ? categoryTally : (categoryTally - labelTally) / categoryTally,
       score: 1,
       attributes: [],
       category: nodes[0].id,
