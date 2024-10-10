f5136ac379e7099b0e4a79cea8bf4ce242e3d46c
Antonio Sánchez <sago.ant@gmail.com>
2024-10-10 10:26:39 +0000

    refactor: avoid sorting by score

--- apps/indexer/src/labeler/interpretations/interpret.ts
+++ apps/indexer/src/labeler/interpretations/interpret.ts
@@ -1,14 +1,29 @@
 import type { MatchedNodes } from "@tissai/db"
-import { normalize } from "./normalize.js"
+import { type Interpretation, normalize } from "./normalize.js"
 import { calculateProbability } from "./calculateProbability.js"
 
 export function interpret(nodes: MatchedNodes) {
-  const interpretations = nodes.map(normalize).flat()
+  return nodes.map(normalize).flat().reduce((best, interpretation) => {
+    const score = 1 + interpretation.properties.filter(p => p.value).length
 
-  return interpretations.map(({ category, properties}) => {
+    if (best.score > score) {
+      return best
+    }
+    if (best.score === score) {
+      return {
+        score: best.score,
+        interpretations: [...best.interpretations, interpretation]
+      }
+    }
+
+    return {
+      score,
+      interpretations: [interpretation],
+    }
+  }, { score: 0, interpretations: [] as Interpretation[] }).interpretations.map(({ category, properties}) => {
     return {
       category: category.name,
       properties: properties.filter(property => property.value).map(({ label, value}) => ({ label: label.name, value: value!.name })) 
     }
-  }).toSorted((a,b) => b.properties.length - a.properties.length)[0]
+  })[0]
 }
