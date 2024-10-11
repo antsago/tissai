6822a7f3a2bc299d77e1749ccb338e75b752080d
Antonio Sánchez <sago.ant@gmail.com>
2024-10-11 08:09:10 +0000

    refactor: extracted updateNetwork

--- apps/indexer/src/trainer/processPage.ts
+++ apps/indexer/src/trainer/processPage.ts
@@ -1,38 +1,7 @@
-import { randomUUID } from "crypto"
 import { NonMatch, Type, type Compiler } from "../parser/index.js"
 import { type OnPage } from "../PageServer/index.js"
-import type { Property, Inference } from "./labeler/index.js"
 import { ProductType } from "./schemas.js"
-import { type Db } from "@tissai/db"
-
-async function updateNetwork({ category, properties }: Inference, db: Db) {
-  const { id: categoryId } = await db.nodes.upsert({
-    id: randomUUID(),
-    parent: null,
-    name: category,
-    tally: 1,
-  })
-
-  await Promise.all(
-    properties.map(
-      async (property: Property) => {
-        const { id: labelId } = await db.nodes.upsert({
-          id: randomUUID(),
-          parent: categoryId,
-          name: property.label,
-          tally: 1,
-        })
-
-        await db.nodes.upsert({
-          id: randomUUID(),
-          parent: labelId,
-          name: property.value,
-          tally: 1,
-        })
-      },
-    ),
-  )
-}
+import { updateNetwork } from "./updateNetwork.js"
 
 export const processPage: OnPage<Compiler> = async (
   page,
---
+++ apps/indexer/src/trainer/updateNetwork.ts
@@ -0,0 +1,32 @@
+import { randomUUID } from "crypto"
+import type { Inference } from "./labeler/index.js"
+import { type Db } from "@tissai/db"
+
+export async function updateNetwork({ category, properties }: Inference, db: Db) {
+  const { id: categoryId } = await db.nodes.upsert({
+    id: randomUUID(),
+    parent: null,
+    name: category,
+    tally: 1,
+  })
+
+  await Promise.all(
+    properties.map(
+      async (property) => {
+        const { id: labelId } = await db.nodes.upsert({
+          id: randomUUID(),
+          parent: categoryId,
+          name: property.label,
+          tally: 1,
+        })
+
+        await db.nodes.upsert({
+          id: randomUUID(),
+          parent: labelId,
+          name: property.value,
+          tally: 1,
+        })
+      },
+    ),
+  )
+}
