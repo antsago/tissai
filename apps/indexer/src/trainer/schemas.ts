62c7f346fb4fe85f1ebc8ec91776ca26a9b3fa04
Antonio Sánchez <sago.ant@gmail.com>
2024-10-11 11:48:45 +0000

    refactor: removed now unused code

--- apps/indexer/src/trainer/schemas.ts
+++ apps/indexer/src/trainer/schemas.ts
@@ -1,36 +1,6 @@
-import { Compiler, Required, Tokenizer, Schema, Type } from "../parser/index.js"
+import { Tokenizer } from "../parser/index.js"
 import type { Reporter } from "../PageServer/index.js"
-import { LLM, label } from "./labeler/index.js"
-
-export const ProductType = Symbol("Product")
-
-const getSchemas =
-  (llm: LLM) =>
-  (tokenizer: Tokenizer): Schema[] => [
-    {
-      [Type]: ProductType,
-      [Required]: {
-        key: "@type",
-        value: "Product",
-      },
-      title: {
-        name: "name",
-        parse: {
-          as: "schemas",
-          with: label(llm, tokenizer),
-        },
-      },
-    },
-  ]
-
-export const compilerFixture = (reporter: Reporter) => {
-  const llm = LLM(reporter)
-  const compiler = Compiler(getSchemas(llm))
-  return [
-    compiler,
-    () => Promise.all([compiler?.close(), llm?.close()]),
-  ] as const
-}
+import { LLM } from "./labeler/index.js"
 
 export const llmFixture = (reporter: Reporter) => {
   const llm = LLM(reporter)
