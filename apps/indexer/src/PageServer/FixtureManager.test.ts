85744fd0b8ef06138b7f5e039b7980ed03dc1db7
Antonio Sánchez <sago.ant@gmail.com>
2024-10-11 09:37:13 +0000

    feat: FixtureManager tests

---
+++ apps/indexer/src/PageServer/FixtureManager.test.ts
@@ -0,0 +1,53 @@
+import { describe, it, beforeEach, vi } from "vitest"
+import { FixtureManager } from "./FixtureManager.js"
+
+describe("FixtureManager", () => {
+  const reporter = {} as any
+  const db = {}
+  const closeDb = vi.fn()
+  const compiler = {}
+  const closeCompiler = vi.fn()
+  const dbFixture = vi.fn()
+  const compilerFixture = vi.fn()
+  
+  let manager: FixtureManager<any>
+  beforeEach(() => {
+    vi.resetAllMocks()
+
+    closeDb.mockResolvedValue(undefined)
+    closeCompiler.mockResolvedValue(undefined)
+    dbFixture.mockResolvedValue([db,closeDb])
+    compilerFixture.mockResolvedValue([compiler,closeCompiler])
+
+    manager = FixtureManager(compilerFixture, dbFixture)
+  })
+
+  it("initializes passed fixtures", async ({ expect }) => {
+    const result = await manager.init(reporter)
+
+    expect(result).toStrictEqual({ db, compiler })
+    expect(dbFixture).toHaveBeenCalledWith(reporter)
+    expect(compilerFixture).toHaveBeenCalledWith(reporter)
+  })
+
+  it("closes initalized fixtures", async ({ expect }) => {
+    await manager.init(reporter)
+
+    await manager.close()
+
+    expect(closeDb).toHaveBeenCalled()
+    expect(closeCompiler).toHaveBeenCalled()
+  })
+
+  it("handles initialization errors", async ({ expect }) => {
+    compilerFixture.mockRejectedValueOnce(new Error("Booh!"))
+
+    try {
+      await manager.init(reporter)
+    } catch {}
+    await manager.close()
+
+    expect(closeDb).toHaveBeenCalled()
+    expect(closeCompiler).not.toHaveBeenCalled()
+  })
+})
--- apps/indexer/src/PageServer/FixtureManager.ts
+++ apps/indexer/src/PageServer/FixtureManager.ts
@@ -1,5 +1,5 @@
 import { type Db } from "@tissai/db"
-import { Reporter } from "./Reporter.js"
+import { type Reporter } from "./Reporter.js"
 
 export type OptionalPromise<T> = Promise<T> | T
 type CloseFixture = () => OptionalPromise<unknown>
@@ -11,6 +11,7 @@ export function FixtureManager<T>(
   compilerFixture: Fixture<T>,
   dbFixture: Fixture<Db>,
 ) {
+  // const close = {} as { compiler?: CloseFixture, db?: CloseFixture }
   let closeDb: CloseFixture
   let closeCompiler: CloseFixture
 
@@ -19,6 +20,9 @@ export function FixtureManager<T>(
       let db, compiler
       ;[db, closeDb] = await dbFixture(reporter)
       ;[compiler, closeCompiler] = await compilerFixture(reporter)
+
+      // close.db = closeDb
+      // close.compiler = closeCompiler
 
       return { db, compiler }
     },
