d835c8958ee03ca1b091afa261e7929ca8952cbb
Antonio Sánchez <sago.ant@gmail.com>
2024-10-14 14:51:06 +0000

    feat: added svelte-kit

---
+++ apps/site/.storybook/main.ts
@@ -0,0 +1,13 @@
+import type { StorybookConfig } from "@storybook/sveltekit";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+  addons: [
+    "@storybook/addon-essentials",
+  ],
+  framework: {
+    name: "@storybook/sveltekit",
+    options: {},
+  },
+};
+export default config;
