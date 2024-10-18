b4e23edb7ae41ee1802bed504326e77e54109150
Antonio Sánchez <sago.ant@gmail.com>
2024-10-18 08:00:11 +0000

    fix: advance index on error

--- apps/indexer/src/Crawler/streamFor.ts
+++ apps/indexer/src/Crawler/streamFor.ts
@@ -4,14 +4,17 @@ export const streamFor = async <T>(
   onError: (error: unknown, current: T) => void,
 ) => {
   let index = 1
+  let sucessfull = 0
   for await (let item of stream) {
     try {
       await onItem(item, index)
-      index += 1
+      sucessfull += 1
     } catch (error) {
       onError(error, item)
+    } finally {
+      index += 1
     }
   }
 
-  return index - 1
+  return sucessfull
 }
