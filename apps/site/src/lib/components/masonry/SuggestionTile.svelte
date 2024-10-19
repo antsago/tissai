473045bf0f1333a43385087836c7540208ed8f19
Antonio Sánchez <sago.ant@gmail.com>
2024-10-19 09:02:38 +0000

    refactor: created masonry folder

--- apps/site/src/lib/components/masonry/SuggestionTile.svelte
+++ apps/site/src/lib/components/masonry/SuggestionTile.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Suggestion } from "@tissai/db"
-  import ChipContainer from "./ChipContainer.svelte"
-  import Chip from "./Chip.svelte"
+  import ChipContainer from "../ChipContainer.svelte"
+  import Chip from "../Chip.svelte"
 
   export let suggestion: Suggestion
   export let baseUrl: string
---
+++ apps/site/src/lib/components/masonry/index.ts
@@ -0,0 +1,3 @@
+export { default as Masonry } from "./Masonry.svelte"
+export { default as ProductTile } from "./ProductTile.svelte"
+export { default as SuggestionTile } from "./SuggestionTile.svelte"
