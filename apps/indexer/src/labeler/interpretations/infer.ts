da37fe4f7f60db08f6162ac5521b0a11adf642c6
Antonio Sánchez <sago.ant@gmail.com>
2024-10-21 12:52:32 +0000

    refactor: improved readability

--- apps/indexer/src/labeler/interpretations/infer.ts
+++ apps/indexer/src/labeler/interpretations/infer.ts
@@ -1,33 +1,36 @@
 import type { Db, MatchedNodes } from "@tissai/db"
-import {
-  type Interpretation,
-  Node,
-  normalize,
-  type Property,
-} from "./normalize.js"
+import { type Interpretation, type Property, normalize } from "./normalize.js"
 import { calculateProbability } from "./calculateProbability.js"
 
-const BASE_SCORE = { score: 0, interpretations: [] as Interpretation[] }
-const pickBestScores = (
-  best: typeof BASE_SCORE,
-  interpretation: Interpretation,
-) => {
-  const score = 1 + interpretation.properties.filter((p) => p.value).length
+function calculateScore(interpretation: Interpretation) {
+  return 1 + interpretation.properties.filter((p) => p.value).length
+}
 
-  if (best.score > score) {
-    return best
-  }
-  if (best.score === score) {
-    return {
-      score: best.score,
-      interpretations: [...best.interpretations, interpretation],
+function pickBestInterpretation(interpretations: Generator<Interpretation>) {
+  let bestInterpretation: Interpretation | undefined
+  let bestProbability: number | undefined
+  let bestScore = 0
+
+  for (let interpretation of interpretations) {
+    const score = calculateScore(interpretation)
+
+    if (!bestInterpretation || bestScore < score) {
+      bestInterpretation = interpretation
+      bestScore = score
+      bestProbability = undefined
+    } else if (bestScore === score) {
+      const probability = calculateProbability(interpretation)
+      bestProbability =
+        bestProbability ?? calculateProbability(bestInterpretation)
+
+      if (bestProbability < probability) {
+        bestInterpretation = interpretation
+        bestProbability = probability
+      }
     }
   }
 
-  return {
-    score,
-    interpretations: [interpretation],
-  }
+  return bestInterpretation
 }
 
 function* normalizeNodes(nodes: MatchedNodes) {
@@ -38,31 +41,12 @@ function* normalizeNodes(nodes: MatchedNodes) {
 
 export async function infer(words: string[], db: Db) {
   const nodes = await db.nodes.match(words)
-
-  let bestScore = 0
-  let bestInterpretation: Interpretation | undefined
-  let bestProbability: number | undefined
-  for (let interpretation of normalizeNodes(nodes)) {
-    const score = 1 + interpretation.properties.filter((p) => p.value).length
-
-    if (!bestInterpretation || bestScore < score) {
-      bestInterpretation = interpretation
-      bestScore = score
-      bestProbability = undefined
-    } else if (bestScore === score) {
-      bestProbability = bestProbability ?? calculateProbability(bestInterpretation)
-      const probability = calculateProbability(interpretation)
-
-      if (bestProbability < probability) {
-        bestInterpretation = interpretation
-        bestProbability = probability
-      }
-    }
-  }
+  const interpretations = normalizeNodes(nodes)
+  const interpretation = pickBestInterpretation(interpretations)
 
   return {
-    category: bestInterpretation?.category,
-    attributes: (bestInterpretation?.properties.filter(
+    category: interpretation?.category,
+    attributes: (interpretation?.properties.filter(
       (property) => property.value,
     ) ?? []) as Required<Property>[],
   }
