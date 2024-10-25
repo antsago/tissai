b4e0e48fdebcf2a23e82d5e07fd91a73c759bd46
Antonio Sánchez <sago.ant@gmail.com>
2024-10-25 16:31:26 +0000

    feat: inferFilters

--- apps/site/src/lib/server/parseSearchParams.test.ts
+++ apps/site/src/lib/server/parseSearchParams.test.ts
@@ -3,13 +3,18 @@ import {
   CATEGORY_NODE,
   LABEL_NODE,
   mockDbFixture,
+  queries,
   VALUE_NODE,
 } from "@tissai/db/mocks"
 import { mockPythonFixture } from "@tissai/python-pool/mocks"
 import { Db } from "@tissai/db"
 import { Tokenizer } from "@tissai/tokenizer"
 import { STRING_ATTRIBUTE } from "mocks"
-import { parseSearchParams, labelFilters } from "./parseSearchParams"
+import {
+  parseSearchParams,
+  labelFilters,
+  inferFilters,
+} from "./parseSearchParams"
 
 const it = test.extend({ db: mockDbFixture, python: mockPythonFixture })
 
@@ -105,6 +110,103 @@ describe("labelFilters", () => {
       },
       attributes: undefined,
     })
+  })
+})
+
+describe("inferFilters", () => {
+  it("infers filters from query", async ({ db, python }) => {
+    const query = `${CATEGORY_NODE.name} ${VALUE_NODE.name}`
+    const WORDS = [
+      {
+        isMeaningful: true,
+        trailing: " ",
+        text: CATEGORY_NODE.name,
+        originalText: CATEGORY_NODE.name,
+      },
+      {
+        isMeaningful: true,
+        trailing: "",
+        text: VALUE_NODE.name,
+        originalText: VALUE_NODE.name,
+      },
+    ]
+    python.mockReturnValue(WORDS)
+    db.pool.query.mockReturnValueOnce({
+      rows: [
+        {
+          name: CATEGORY_NODE.name,
+          id: CATEGORY_NODE.id,
+          tally: CATEGORY_NODE.tally,
+          children: [
+            {
+              name: LABEL_NODE.name,
+              id: LABEL_NODE.id,
+              tally: LABEL_NODE.tally,
+              children: [
+                {
+                  name: VALUE_NODE.name,
+                  id: VALUE_NODE.id,
+                  tally: VALUE_NODE.tally,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    })
+
+    const result = await inferFilters(query, {
+      db: Db(),
+      tokenizer: Tokenizer(),
+    })
+
+    expect(result).toStrictEqual({
+      category: {
+        label: "categoría",
+        id: CATEGORY_NODE.id,
+        name: CATEGORY_NODE.name,
+      },
+      attributes: [
+        {
+          label: LABEL_NODE.name,
+          id: VALUE_NODE.id,
+          name: VALUE_NODE.name,
+        },
+      ],
+    })
+    expect(python.worker.send).toHaveBeenCalledWith(query)
+    expect(db).toHaveExecuted(
+      queries.nodes.match([CATEGORY_NODE.name, VALUE_NODE.name]),
+    )
+  })
+
+  it("handles no infered category", async ({ db, python }) => {
+    const query = "foo bar"
+    const WORDS = [
+      {
+        isMeaningful: true,
+        trailing: " ",
+        text: "foo",
+        originalText: "foo",
+      },
+      {
+        isMeaningful: true,
+        trailing: "",
+        text: "bar",
+        originalText: "bar",
+      },
+    ]
+    python.mockReturnValue(WORDS)
+    db.pool.query.mockReturnValueOnce({
+      rows: [],
+    })
+
+    const result = await inferFilters(query, {
+      db: Db(),
+      tokenizer: Tokenizer(),
+    })
+
+    expect(result).toStrictEqual({})
   })
 })
 
--- apps/site/src/lib/server/parseSearchParams.ts
+++ apps/site/src/lib/server/parseSearchParams.ts
@@ -27,6 +27,31 @@ export async function labelFilters(
   }
 }
 
+export async function inferFilters(query: string, locals: App.Locals) {
+  const words = await locals.tokenizer.fromText(query)
+  const infered = await infer(
+    words.filter((w) => w.isMeaningful).map((w) => w.text),
+    locals.db,
+  )
+
+  if (!infered.category) {
+    return {}
+  }
+
+  return {
+    category: {
+      label: "categoría",
+      id: infered.category.id,
+      name: infered.category.name,
+    },
+    attributes: infered.attributes.map((att) => ({
+      name: att.value.name,
+      label: att.label.name,
+      id: att.value.id,
+    })),
+  }
+}
+
 export async function parseSearchParams(
   params: URLSearchParams,
   locals: App.Locals,
