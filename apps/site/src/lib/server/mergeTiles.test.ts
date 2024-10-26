42189314d2e9255e5b8b2888496d3a5d3681a052
Antonio Sánchez <sago.ant@gmail.com>
2024-10-26 11:16:49 +0000

    feat: merge suggestions and products

--- apps/site/src/lib/server/mergeTiles.test.ts
+++ apps/site/src/lib/server/mergeTiles.test.ts
@@ -24,51 +24,39 @@ describe("mergeTiles", () => {
     expect(result).toStrictEqual(expect.arrayContaining(products))
   })
 
-  // it("ignores excess suggestions", async () => {
-  //   const suggestions = [
-  //     {
-  //       label: ATTRIBUTE.label,
-  //       values: [ATTRIBUTE.value],
-  //       frequency: 1,
-  //     },
-  //   ]
-  //   const products = [SEARCH_RESULT]
+  it("interpolates multiple suggestions", async () => {
+    const suggestions = new Array(5).fill(undefined).map((_, i) => ({
+      label: `${SUGGESTION.label}_${i}`,
+      values: SUGGESTION.values,
+    }))
+    const products = new Array(20).fill(undefined).map((_, i) => ({
+      ...SEARCH_RESULT,
+      id: String(i),
+    }))
 
-  //   const result = mergeTiles(products, suggestions)
+    const result = mergeTiles(products, suggestions)
 
-  //   expect(result).toStrictEqual(products)
-  // })
+    expect(result).toHaveLength(products.length + suggestions.length)
+    expect(result).toStrictEqual(expect.arrayContaining(products))
+    expect(result[0]).toStrictEqual(suggestions[0])
+    expect(result[5]).toStrictEqual(suggestions[1])
+    expect(result[10]).toStrictEqual(suggestions[2])
+    expect(result[15]).toStrictEqual(suggestions[3])
+    expect(result[20]).toStrictEqual(suggestions[4])
+  })
 
-  // it.each([
-  //   { frequency: 1, position: 4 },
-  //   { frequency: 0.811, position: 4 },
-  //   { frequency: 0.8, position: 3 },
-  //   { frequency: 0.611, position: 3 },
-  //   { frequency: 0.6, position: 2 },
-  //   { frequency: 0.411, position: 2 },
-  //   { frequency: 0.4, position: 1 },
-  //   { frequency: 0.211, position: 1 },
-  //   { frequency: 0.2, position: 0 },
-  //   { frequency: 0.001, position: 0 },
-  // ])(
-  //   "puts suggestions with frequency $frequency in index $position for every 4 products",
-  //   ({ frequency, position }) => {
-  //     const suggestion = {
-  //       label: ATTRIBUTE.label,
-  //       values: [ATTRIBUTE.value],
-  //       frequency,
-  //     }
-  //     const products = new Array(4).fill(undefined).map((_, i) => ({
-  //       ...SEARCH_RESULT,
-  //       id: String(i),
-  //       title: "Title",
-  //     }))
+  it("ignores excess suggestions", async () => {
+    const suggestions = new Array(5).fill(undefined).map((_, i) => ({
+      label: `${SUGGESTION.label}_${i}`,
+      values: SUGGESTION.values,
+    }))
+    const products = [SEARCH_RESULT]
 
-  //     const result = mergeTiles(products, [suggestion])
+    const result = mergeTiles(products, suggestions)
 
-  //     expect(result).toHaveLength(5)
-  //     expect(result).toStrictEqual(expect.arrayContaining(products))
-  //     expect(result[position]).toStrictEqual(suggestion)
-  //   },
-  // )
+    expect(result).toStrictEqual([
+      suggestions[0],
+      SEARCH_RESULT,
+    ])
+  })
 })
--- apps/site/src/lib/server/mergeTiles.ts
+++ apps/site/src/lib/server/mergeTiles.ts
@@ -1,7 +1,28 @@
 import type { Search, Suggestion } from "@tissai/db"
 
+const SUGGESTION_DISTANCE = 5
+
 function mergeTiles(products: Search[], suggestions: Suggestion[]) {
-  return [...suggestions, ...products]
+  let productIndex = 0
+  let suggestionIndex = 0
+
+  return new Array(suggestions.length + products.length).fill(undefined)
+    .map((_, index) => {
+      if (productIndex >= products.length) {
+        return
+      }
+
+      if (suggestionIndex < suggestions.length && index % SUGGESTION_DISTANCE === 0) {
+        const tile = suggestions[suggestionIndex]
+        suggestionIndex += 1
+        return tile
+      }
+
+      const tile = products[productIndex]
+      productIndex += 1
+      return tile
+    })
+    .filter(tile => tile !== undefined)
 }
 
 export default mergeTiles
