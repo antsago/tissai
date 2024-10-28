fde3222d33734e8839a27d3ea58c1ef326c9d694
Antonio Sánchez <sago.ant@gmail.com>
2024-10-28 14:27:42 +0000

    refactor: extracted first version of encodeParams

--- apps/site/src/lib/server/getSuggestions.ts
+++ apps/site/src/lib/server/getSuggestions.ts
@@ -1,5 +1,18 @@
 import type { Db, Filters } from "@tissai/db"
 
+const encodeParams = (query: string, filters: Filters) => {
+  const params = new URLSearchParams()
+
+  params.append("q", query)
+  params.append("cat", filters.category!.id)
+
+  if(filters.attributes) {
+    params.append("att", filters.attributes?.[0].id)
+  }
+
+  return params.toString()
+}
+
 export const getSuggestions = async (query: string, filters: Filters, db: Db) => {
   const suggestions = filters.category
     ? await db.suggestions.attributes(filters.category.id)
@@ -9,11 +22,22 @@ export const getSuggestions = async (query: string, filters: Filters, db: Db) =>
     .filter((s) => !!s.label && !!s.values?.length)
     .map((s) => ({
       ...s,
-      values: s.values.map(v => ({
+      values: s.values.map(v => {
+        const newFilters = filters.category ? {
+          ...filters,
+          attributes: [...filters.attributes ?? [], { label: s.label, name: v.name!, id: v.id! }]
+        } : {
+          ...filters,
+          category: {
+            name: v.name!,
+            id: v.id!,
+          },
+        }
+
+        return ({
         name: v.name!,
-        href: filters.category ?
-         `/search?q=${encodeURIComponent(query)}&cat=${encodeURIComponent(filters.category!.id)}&att=${encodeURIComponent(v.id!)}`
-        :`/search?q=${encodeURIComponent(query)}&cat=${encodeURIComponent(v.id!)}`
-      }))
+        href: `/search?${encodeParams(query, newFilters)}`
+      })
+      })
     }))
 }
