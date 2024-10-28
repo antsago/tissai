69d42f0451743b98ad1c2a3ba9b96826a2c09091
Antonio Sánchez <sago.ant@gmail.com>
2024-10-28 15:24:42 +0000

    feat: completed encodeParams

--- apps/site/src/lib/server/encodeParams.test.ts
+++ apps/site/src/lib/server/encodeParams.test.ts
@@ -2,65 +2,46 @@ import { describe, it, expect, beforeEach } from "vitest"
 import { PRODUCT } from "@tissai/db/mocks"
 import { encodeParams } from "./encodeParams"
 
-describe.skip("encodeParams", () => {
+describe("encodeParams", () => {
   let params: URLSearchParams
   beforeEach(() => {
     params = new URLSearchParams()
   })
 
-  it("handles empty search", async () => {
-    const query = "the query"
-    const result = encodeParams("busco un pantalón", {
-    })
-
-    expect(result).toStrictEqual({ query: "" })
-  })
-
-  it("parses parameters", async () => {
-    const min = 11.1
-    const max = 22.2
-    const brand = "a brand"
-    const category = "80be7c8e-26dd-47e8-8325-f4bac459dae1"
-    const attributes = ["bc82024f-4532-4f98-ba66-6701f83f76ac"]
-    params.append("q", PRODUCT.title)
-    params.append("min", String(min))
-    params.append("max", String(max))
-    params.append("brand", brand)
-    params.append("cat", category)
-    params.append("att", attributes[0])
+  it("encodes url parameters", async () => {
+    const params = {
+      query: "busco un pantalón",
+      brand: "a brand",
+      min: 11.1,
+      max: 22.2,
+      category: "80be7c8e-26dd-47e8-8325-f4bac459dae1",
+      attributes: ["bc82024f-4532-4f98-ba66-6701f83f76ac"],
+    }
 
     const result = encodeParams(params)
 
-    expect(result).toStrictEqual({
-      query: PRODUCT.title,
-      brand,
-      min,
-      max,
-      category,
-      attributes,
-    })
+    expect(result).toStrictEqual(`q=busco+un+pantal%C3%B3n&brand=a+brand&max=${params.max}&min=${params.min}&cat=${params.category}&att=${params.attributes[0]}`)
   })
 
   it("supports multiple attributes", async () => {
-    const attributes = [
-      "bc82024f-4532-4f98-ba66-6701f83f76ac",
-      "b24cfb54-c160-4190-9715-deeb0c7798ac",
-    ]
-    params.append("att", attributes[0])
-    params.append("att", attributes[1])
+    const params = {
+      query: "",
+      attributes: [
+        "bc82024f-4532-4f98-ba66-6701f83f76ac",
+        "b24cfb54-c160-4190-9715-deeb0c7798ac",
+      ],
+    }
 
     const result = encodeParams(params)
 
-    expect(result.attributes).toStrictEqual(attributes)
+    expect(result).toStrictEqual(`q=&att=${params.attributes[0]}&att=${params.attributes[1]}`)
   })
 
-  it("ignores other parameters", async () => {
-    params.append("q", PRODUCT.title)
-    params.append("foo", "bar")
+  it("handles empty search", async () => {
+    const params = { query: "" }
 
     const result = encodeParams(params)
 
-    expect(result).toStrictEqual({ query: PRODUCT.title })
+    expect(result).toStrictEqual("q=")
   })
-
 })
--- apps/site/src/lib/server/encodeParams.ts
+++ apps/site/src/lib/server/encodeParams.ts
@@ -4,11 +4,19 @@ export const encodeParams = (params: UrlParams) => {
   const encoded = new URLSearchParams()
 
   encoded.append("q", params.query)
-  encoded.append("cat", params.category!)
-
-  if(params.attributes) {
-    encoded.append("att", params.attributes[0])
+  if (params.brand) {
+    encoded.append("brand", params.brand)
   }
+  if (params.max) {
+    encoded.append("max", String(params.max))
+  }
+  if (params.min) {
+    encoded.append("min", String(params.min))
+  }
+  if (params.category) {
+    encoded.append("cat", params.category)
+  }
+  params.attributes?.forEach(attribute => encoded.append("att", attribute))
 
   return encoded.toString()
 }
