4db31db78085af4fdabe0890badfb213b810f902
Antonio Sánchez <sago.ant@gmail.com>
2024-10-28 15:02:33 +0000

    refactor: improved getSuggestions readability

--- apps/site/src/lib/server/getSuggestions.ts
+++ apps/site/src/lib/server/getSuggestions.ts
@@ -1,11 +1,39 @@
 import type { Db, Filters } from "@tissai/db"
 import { encodeParams } from "./encodeParams"
+import type { UrlParams } from "./decodeParams"
 
-export const getSuggestions = async (query: string, filters: Filters, db: Db) => {
-  const suggestions = filters.category
-    ? await db.suggestions.attributes(filters.category.id)
-    : [await db.suggestions.category()]
+const getCategorySuggestion = async (baseParams: UrlParams, db: Db) => {
+  const suggestion = await db.suggestions.category()
   
+  return {
+    label: suggestion.label,
+    values: suggestion.values?.map(value => ({
+      name: value.name,
+      params: {
+        ...baseParams,
+        category: value.id
+      }
+    }))
+  }
+}
+
+type WithCategory = Omit<UrlParams, "category"> & Required<Pick<UrlParams, "category">>
+const getAttributeSuggestions = async (baseParams: WithCategory, db: Db) => {
+  const suggestions = await db.suggestions.attributes(baseParams.category)
+  
+  return suggestions.map(suggestion => ({
+    label: suggestion.label,
+    values: suggestion.values?.map(value => ({
+      name: value.name!,
+      params: {
+        ...baseParams,
+        attributes: [...baseParams.attributes ?? [], value.id!],
+      }
+    }))
+  }))
+}
+
+export async function getSuggestions(query: string, filters: Filters, db: Db) {
   const baseParams = {
     query,
     ...filters,
@@ -13,23 +41,17 @@ export const getSuggestions = async (query: string, filters: Filters, db: Db) =>
     attributes: filters.attributes?.map(a => a.id)
   }
 
+  const suggestions = baseParams.category === undefined
+    ? [await getCategorySuggestion(baseParams, db)]
+    : await getAttributeSuggestions(baseParams as any, db)
+  
   return suggestions
     .filter((s) => !!s.label && !!s.values?.length)
     .map((s) => ({
       ...s,
-      values: s.values.map(v => {
-        const params = filters.category ? {
-          ...baseParams,
-          attributes: [...baseParams.attributes ?? [], v.id!],
-        } : {
-          ...baseParams,
-          category: v.id!,
-        }
-
-        return ({
-        name: v.name!,
-        href: `/search?${encodeParams(params)}`
-      })
-      })
+      values: s.values.map(v => ({
+        name: v.name,
+        href: `/search?${encodeParams(v.params)}`
+      }))
     }))
 }
