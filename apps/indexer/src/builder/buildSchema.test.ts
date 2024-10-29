068eefbe7d38ec51544e73e559f3537dbee29e68
Antonio Sánchez <sago.ant@gmail.com>
2024-10-29 23:45:20 +0000

    feat: parent categories based on common words

--- apps/indexer/src/builder/buildSchema.test.ts
+++ apps/indexer/src/builder/buildSchema.test.ts
@@ -3,20 +3,29 @@ import { buildSchema } from "./buildSchema.js"
 
 describe("buildSchema", () => {
   it("recognizes new titles as categories", () => {
-    const titles = ["jeans cropped marine azul", "jegging encerado vila"]
+    const titles = ["jeans cropped", "jegging"]
 
     const result = buildSchema(titles)
 
     expect(result).toStrictEqual([
       {
-        name: titles[0],
-        properties: [],
-        subcategories: [],
+        name: ["jeans", "cropped"],
       },
       {
-        name: titles[1],
-        properties: [],
-        subcategories: [],
+        name: ["jegging"],
+      },
+    ])
+  })
+
+  it("common initial words become a parent category", () => {
+    const titles = ["jeans cropped azul", "jeans cropped camel"]
+
+    const result = buildSchema(titles)
+
+    expect(result).toStrictEqual([
+      {
+        name: ["jeans", "cropped"],
+        categories: [{ name: ["azul"] }, { name: ["camel"] }],
       },
     ])
   })
--- apps/indexer/src/builder/buildSchema.ts
+++ apps/indexer/src/builder/buildSchema.ts
@@ -1,7 +1,51 @@
+type Schema = { name: string[], categories?: Schema[] }
+
+const commonWordsFor = (a: string[], b: string[]) => {
+  let common = [] as string[]
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      return common
+    }
+
+    common = [...common, a[i]]
+  }
+  
+  return common
+}
+
 export function buildSchema(titles: string[]) {
-  return titles.map(title => ({
-    name: title,
-    properties: [],
-    subcategories: [],
-  }))
+  let categories = [] as Schema[]
+
+  for (let title of titles) {
+    const words = title.split(" ")
+
+    let commonWords = [] as string[]
+    let commonCategory
+    for (let category of categories) {
+      commonWords = commonWordsFor(words, category.name)
+
+      if (commonWords.length) {
+        commonCategory = category
+        break
+      }
+    }
+
+    if (!commonCategory) {
+      categories = [...categories, { name: words }]
+    } else {
+      categories = [{
+        name: commonWords,
+        categories: [
+          {
+            name: commonCategory.name.slice(commonWords.length),
+          },
+          {
+            name: words.slice(commonWords.length),
+          },
+        ]
+      }]
+    }
+  }
+
+  return categories
 }
