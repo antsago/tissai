4a34685815094dcc4cde2fe2eef110ef6ee642f2
Antonio Sánchez <sago.ant@gmail.com>
2024-10-30 22:52:43 +0000

    feat: new, failing, tests

--- apps/indexer/src/builder/buildSchema.test.ts
+++ apps/indexer/src/builder/buildSchema.test.ts
@@ -2,6 +2,20 @@ import { expect, describe, it } from "vitest"
 import { buildSchema } from "./buildSchema.js"
 
 describe("buildSchema", () => {
+  it.skip("works", () => {
+    const titles = [
+      "Jeans cropped marine azul",
+      "Jeans cropped marine camel",
+      "Jeans cropped marine negro",
+      "Jeans Culotte Lavado Sostenible",
+      "Jeans Culotte Lavado Sostenible",
+    ]
+
+    const result = buildSchema(titles)
+
+    expect(result).toStrictEqual([])
+  })
+
   it("recognizes new titles as categories", () => {
     const titles = ["jeans cropped", "jegging"]
 
@@ -42,6 +56,19 @@ describe("buildSchema", () => {
       {
         name: ["jeans"],
         categories: [{ name: ["azul"] }, { name: ["camel"] }],
+      },
+    ])
+  })
+
+  it("recognizes new categories", () => {
+    const titles = ["jeans cropped azul", "jeans cropped camel", "jeans cropped negro"]
+
+    const result = buildSchema(titles)
+
+    expect(result).toStrictEqual([
+      {
+        name: ["jeans", "cropped"],
+        categories: [{ name: ["azul"] }, { name: ["camel"] }, { name: ["negro"]}],
       },
     ])
   })
