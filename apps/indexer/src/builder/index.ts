2ec9f25eb78011ef264f850b621a7202c2a841cc
Antonio Sánchez <sago.ant@gmail.com>
2024-11-27 11:01:42 +0000

    feat: run for all titles

---
+++ apps/indexer/src/builder/index.ts
@@ -0,0 +1,21 @@
+import { addToSchema } from "./addToSchema.js"
+import { type TreeNode, Schema } from "./nodesToSchema.js"
+import titles from "./titles.js"
+
+const normalizeString = (str: string) =>
+  str
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")
+    .toLowerCase()
+
+const initialSchema = {
+  name: [],
+  properties: [],
+  children: [],
+} as TreeNode
+
+const finalSchema = titles
+  .map((title) => normalizeString(title))
+  .reduce((schema, title) => addToSchema(title, schema), Schema(initialSchema))
+
+console.log(JSON.stringify(finalSchema.asTree(), null, 2))
