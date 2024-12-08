9ac6a7e674bf19f846e4f8bfbe084ec12a040b8a
Antonio Sánchez <sago.ant@gmail.com>
2024-12-08 14:20:52 +0000

    feat: added word indexes to spans

--- apps/indexer/src/builder/extractValues.ts
+++ apps/indexer/src/builder/extractValues.ts
@@ -11,6 +11,8 @@ type Values = Record<
 
 type Span = {
   nodeId?: UUID
+  start?: number
+  end?: number
   words: string[]
 }
 
@@ -22,16 +24,24 @@ function matchTitle(title: string, values: Values): Span[] {
 
       return {
         nodeId: match?.id,
+        index: match?.name.findIndex((w) => w === word),
         word,
       }
     })
     .reduce((mergedSpans, wordSpan) => {
       const previousSpan = mergedSpans.at(-1)
-      if (previousSpan && previousSpan.nodeId === wordSpan.nodeId) {
+      if (
+        previousSpan &&
+        previousSpan.nodeId === wordSpan.nodeId &&
+        ((previousSpan.end === undefined && wordSpan.index === undefined) || (
+          previousSpan.end !== undefined && wordSpan.index !== undefined && previousSpan.end + 1 === wordSpan.index))
+      ) {
         return [
           ...mergedSpans.slice(0, -1),
           {
             nodeId: previousSpan.nodeId,
+            start: previousSpan.start,
+            end: wordSpan.index,
             words: [...previousSpan.words, wordSpan.word],
           },
         ]
@@ -41,6 +51,8 @@ function matchTitle(title: string, values: Values): Span[] {
         ...mergedSpans,
         {
           nodeId: wordSpan.nodeId,
+          start: wordSpan.index,
+          end: wordSpan.index,
           words: [wordSpan.word],
         },
       ]
