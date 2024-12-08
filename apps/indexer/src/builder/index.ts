093924f88772a85d90408f07359fe5f1ed77ffb5
Antonio Sánchez <sago.ant@gmail.com>
2024-12-08 17:15:39 +0000

    feat: run extractValues on all titles

---
+++ apps/indexer/src/builder/index.ts
@@ -0,0 +1,14 @@
+import _ from "lodash"
+import { extractValues } from "./extractValues.js"
+import titles from "./titles.js"
+
+const normalizeString = (str: string) =>
+  str
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")
+    .toLowerCase()
+
+const values = extractValues(titles
+  .map((title) => normalizeString(title)))
+
+console.log(JSON.stringify(values, null, 2))
